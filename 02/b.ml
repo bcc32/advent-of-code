cc946814404706a8bf1c3efa125efbfba13319a5
Aaron L. Zeng <me@bcc32.com>
2017-12-02 00:16:06 -0500

    Day 2

---
+++ 02/b.ml
@@ -0,0 +1,22 @@
+open! Core
+
+let get_whole_division nums =
+  List.find_map nums ~f:(fun a ->
+    List.find_map nums ~f:(fun b ->
+      if a <> b && a % b = 0
+      then (Some (a / b))
+      else None))
+;;
+
+let () =
+  In_channel.with_file Sys.argv.(1) ~f:(fun file ->
+    In_channel.input_lines file
+    |> List.sum (module Int) ~f:(fun row ->
+      let nums =
+        String.split row ~on:'\t'
+        |> List.map ~f:Int.of_string
+      in
+      get_whole_division nums
+      |> Option.value_exn)
+    |> printf "%d\n")
+;;
