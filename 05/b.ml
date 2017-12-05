92f1fc14352fd5fc8ad5187a4620657bc42a6cbb
Aaron L. Zeng <me@bcc32.com>
2017-12-05 00:14:14 -0500

    Day 5

---
+++ 05/b.ml
@@ -0,0 +1,28 @@
+open! Core
+
+let simulate steps =
+  let n = Array.length steps in
+  let rec loop index count =
+    if index < 0 || index >= n
+    then count
+    else (
+      let step = steps.( index ) in
+      steps.( index ) <- (
+        if step >= 3
+        then (step - 1)
+        else (step + 1));
+      loop (index + step) (count + 1))
+  in
+  loop 0 0
+;;
+
+let () =
+  let steps =
+    In_channel.with_file Sys.argv.(1) ~f:(fun file ->
+      In_channel.input_lines file
+      |> List.to_array
+      |> Array.map ~f:Int.of_string)
+  in
+  simulate steps
+  |> printf "%d\n"
+;;
