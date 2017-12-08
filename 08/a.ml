24bb57ab66df942113c529211eb73f6cd1e95a57
Aaron L. Zeng <me@bcc32.com>
2017-12-08 00:08:19 -0500

    Day 8

---
+++ 08/a.ml
@@ -0,0 +1,39 @@
+open! Core
+
+let relational_of_string =
+  function
+  | "<" -> Int.(<)
+  | ">" -> Int.(>)
+  | ">=" -> Int.(>=)
+  | "<=" -> Int.(<=)
+  | "==" -> Int.(=)
+  | "!=" -> Int.(<>)
+  | s -> invalid_arg s
+;;
+
+let () =
+  let input =
+    In_channel.with_file Sys.argv.(1) ~f:(fun file ->
+      In_channel.input_lines file
+      |> List.map ~f:(fun line ->
+        match String.split line ~on:' ' with
+        | [ reg; ins; amt; _; other; rel; lit ] ->
+          ( reg
+          , (if ins = "inc" then 1 else -1)
+          , Int.of_string amt
+          , other
+          , relational_of_string rel
+          , Int.of_string lit)
+        | _ -> invalid_arg line))
+  in
+  let registers = String.Table.create () in
+  List.iter input ~f:(fun (reg, factor, amt, other, f, lit) ->
+    let other = Hashtbl.find_or_add registers other ~default:(fun () -> 0) in
+    let reg_cur = Hashtbl.find_or_add registers reg ~default:(fun () -> 0) in
+    if f other lit
+    then (Hashtbl.set registers ~key:reg ~data:(factor * amt + reg_cur)));
+  Hashtbl.data registers
+  |> List.max_elt ~cmp:Int.compare
+  |> uw
+  |> printf "%d\n"
+;;
---
+++ 08/b.ml
@@ -0,0 +1,41 @@
+open! Core
+
+let relational_of_string =
+  function
+  | "<" -> Int.(<)
+  | ">" -> Int.(>)
+  | ">=" -> Int.(>=)
+  | "<=" -> Int.(<=)
+  | "==" -> Int.(=)
+  | "!=" -> Int.(<>)
+  | s -> invalid_arg s
+;;
+
+let () =
+  let input =
+    In_channel.with_file Sys.argv.(1) ~f:(fun file ->
+      In_channel.input_lines file
+      |> List.map ~f:(fun line ->
+        match String.split line ~on:' ' with
+        | [ reg; ins; amt; _; other; rel; lit ] ->
+          ( reg
+          , (if ins = "inc" then 1 else -1)
+          , Int.of_string amt
+          , other
+          , relational_of_string rel
+          , Int.of_string lit)
+        | _ -> invalid_arg line))
+  in
+  let registers = String.Table.create () in
+  let max = ref 0 in
+  List.iter input ~f:(fun (reg, factor, amt, other, f, lit) ->
+    let other = Hashtbl.find_or_add registers other ~default:(fun () -> 0) in
+    let reg_cur = Hashtbl.find_or_add registers reg ~default:(fun () -> 0) in
+    if f other lit
+    then (
+      let data = factor * amt + reg_cur in
+      Hashtbl.set registers ~key:reg ~data;
+      if data > !max
+      then (max := data)));
+  printf "%d\n" !max
+;;
