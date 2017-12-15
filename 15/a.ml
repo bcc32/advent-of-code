c2cffebaa7a0e2fc880ed9ffd72bf4d2d06dcfeb
Aaron L. Zeng <me@bcc32.com>
2017-12-15 00:05:38 -0500

    Day 15

---
+++ 15/a.ml
@@ -0,0 +1,28 @@
+open! Core
+
+let modulo = 2147483647
+
+let a_factor = 16807
+let b_factor = 48271
+
+let () =
+  let input =
+    In_channel.with_file Sys.argv.(1) ~f:In_channel.input_lines
+    |> List.map ~f:(fun line ->
+      String.subo line ~pos:24
+      |> Int.of_string)
+  in
+  let count = ref 0 in
+  match input with
+  | [ a; b ] ->
+    let a = ref a in
+    let b = ref b in
+    for _ = 1 to 40_000_000 do
+      a := !a * a_factor % modulo;
+      b := !b * b_factor % modulo;
+      if (!a % (1 lsl 16)) = (!b % (1 lsl 16))
+      then (incr count)
+    done;
+    printf "%d\n" !count
+  | _ -> invalid_arg "input count"
+;;
