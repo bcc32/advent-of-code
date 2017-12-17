b7ae085625a92ee366bf8c304ed3040be09f52ff
Aaron L. Zeng <me@bcc32.com>
2017-12-16 23:13:19 -0600

    Day 17

---
+++ 17/a.ml
@@ -0,0 +1,22 @@
+open! Core
+
+let () =
+  let input =
+    In_channel.with_file Sys.argv.(1) ~f:In_channel.input_all
+    |> String.strip
+    |> Int.of_string
+  in
+  let buf = ref (Array.create 0 ~len:1) in
+  let pos = ref 0 in
+  for next = 1 to 2017 do
+    pos := (!pos + input) % Array.length !buf;
+    let buf' = Array.create 0 ~len:(1 + Array.length !buf) in
+    Array.blit ~src:!buf ~dst:buf' ~src_pos:0 ~dst_pos:0 ~len:(!pos + 1);
+    buf'.(!pos + 1) <- next;
+    Array.blito () ~src:!buf ~dst:buf' ~src_pos:(!pos + 1) ~dst_pos:(!pos + 2);
+    buf := buf';
+    incr pos
+  done;
+  !buf.(!pos + 1)
+  |> printf "%d\n"
+;;
---
+++ 17/b.ml
@@ -0,0 +1,19 @@
+open! Core
+
+let () =
+  let input =
+    In_channel.with_file Sys.argv.(1) ~f:In_channel.input_all
+    |> String.strip
+    |> Int.of_string
+  in
+  let pos = ref 0 in
+  let last_after_zero = ref 1 in
+  for next = 1 to 50_000_000 do
+    pos := (!pos + input) % next;
+    if !pos = 0
+    then (last_after_zero := next);
+    incr pos
+  done;
+  !last_after_zero
+  |> printf "%d\n"
+;;
