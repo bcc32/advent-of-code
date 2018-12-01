0ab5ed94ad79f795288cb53559d52e8a558c42e0
Aaron L. Zeng <me@bcc32.com>
2018-12-01 12:49:09 -0500

    Initial commit, 01a

---
+++ 01/a.ml
@@ -0,0 +1,19 @@
+open! Core
+open! Async
+
+let main () =
+  Reader.with_file "a" ~f:(fun r ->
+    Reader.lines r
+    |> Pipe.fold_without_pushback ~init:0 ~f:(fun acc line -> acc + Int.of_string line))
+  >>| printf "%d\n"
+;;
+
+let command =
+  Command.async
+    ~summary:"01-a"
+    (let open Command.Let_syntax in
+     let%map_open () = return () in
+     fun () -> main ())
+;;
+
+let () = Command.run command
