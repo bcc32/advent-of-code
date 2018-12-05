92347691655c7f0b9fef66584da0b1c7211c472a
Aaron L. Zeng <me@bcc32.com>
2018-12-04 21:49:42 -0500

    Add basic IO to template files

--- template/a.ml
+++ template/a.ml
@@ -3,6 +3,9 @@ open! Async
 open! Import
 
 let main () =
+  let%bind _lines =
+    Reader.with_file "input" ~f:(fun r -> r |> Reader.lines |> Pipe.to_list)
+  in
   printf "output\n";
   return ()
 ;;
