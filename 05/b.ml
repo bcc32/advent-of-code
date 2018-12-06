8e9cec346b80baff109a3837dc7c5fd9052dd894
Aaron L. Zeng <me@bcc32.com>
2018-12-05 19:07:59 -0500

    5b

--- 05/b.ml
+++ 05/b.ml
@@ -3,14 +3,25 @@ open! Async
 open! Import
 
 let main () =
-  let%bind _lines =
+  match%bind
     Reader.with_file "input" ~f:(fun r -> r |> Reader.lines |> Pipe.to_list)
-  in
-  printf "output\n";
-  return ()
+  with
+  | [ polymer ] ->
+    let lower = Char.all |> List.filter ~f:Char.is_lowercase in
+    lower
+    |> List.map ~f:(fun unit ->
+      let unit_removed =
+        polymer |> String.filter ~f:(fun c -> Char.O.(Char.lowercase c <> unit))
+      in
+      String.length (Polymer.react unit_removed))
+    |> List.min_elt ~compare:[%compare: int]
+    |> Option.value_exn
+    |> printf "%d\n";
+    return ()
+  | _ -> failwith "invalid input"
 ;;
 
 let%expect_test "b" =
   let%bind () = main () in
-  [%expect {| output |}]
+  [%expect {| 4178 |}]
 ;;
