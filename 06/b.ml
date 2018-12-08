40d42d0c2261124791fc5428300126a3cd89be61
Aaron L. Zeng <me@bcc32.com>
2018-12-08 01:13:29 -0500

    6b

--- 06/b.ml
+++ 06/b.ml
@@ -2,15 +2,43 @@ open! Core
 open! Async
 open! Import
 
+let re = Re.(compile (seq [ group (rep1 digit); str ", "; group (rep1 digit) ]))
+let dist (x, y) (x', y') = Int.abs (x - x') + Int.abs (y - y')
+let max_dist = 10_000
+
 let main () =
-  let%bind _lines =
-    Reader.with_file "input" ~f:(fun r -> r |> Reader.lines |> Pipe.to_list)
+  let%bind points =
+    Reader.with_file "input" ~f:(fun r ->
+      r
+      |> Reader.lines
+      |> Pipe.map ~f:(fun line ->
+        let group = Re.exec re line in
+        let x = Re.Group.get group 1 |> Int.of_string in
+        let y = Re.Group.get group 2 |> Int.of_string in
+        x, y)
+      |> Pipe.to_list)
   in
-  printf "output\n";
+  let min_x, max_x, min_y, max_y =
+    let x0, y0 = List.hd_exn points in
+    List.fold
+      points
+      ~init:(x0, x0, y0, y0)
+      ~f:(fun (min_x, max_x, min_y, max_y) (x, y) ->
+        Int.min min_x x, Int.max max_x x, Int.min min_y y, Int.max max_y y)
+  in
+  let count = ref 0 in
+  let dist_sum p = List.sum (module Int) points ~f:(dist p) in
+  let margin = max_dist / List.length points in
+  for x = min_x - margin to max_x + margin do
+    for y = min_y - margin to max_y + margin do
+      if dist_sum (x, y) < max_dist then incr count
+    done
+  done;
+  printf "%d\n" !count;
   return ()
 ;;
 
 let%expect_test "b" =
   let%bind () = main () in
-  [%expect {| output |}]
+  [%expect {| 36216 |}]
 ;;
