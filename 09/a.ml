b84f481cfbf109eebb669166c0ea4e7180e719ed
Aaron L. Zeng <me@bcc32.com>
2018-12-09 02:32:10 -0500

    9a

---
+++ 09/a.ml
@@ -0,0 +1,15 @@
+open! Core
+open! Async
+open! Import
+
+let main () =
+  let%bind game = Game.get () in
+  let scores = Game.simulate game in
+  Array.max_elt scores ~compare:Int.compare |> Option.value_exn |> printf "%d\n";
+  return ()
+;;
+
+let%expect_test "a" =
+  let%bind () = main () in
+  [%expect {| 402398 |}]
+;;
