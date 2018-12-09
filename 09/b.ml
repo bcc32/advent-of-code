07ed4bccde48289d004d1beb70e305349b56a781
Aaron L. Zeng <me@bcc32.com>
2018-12-09 02:33:31 -0500

    9b

--- 09/b.ml
+++ 09/b.ml
@@ -3,14 +3,14 @@ open! Async
 open! Import
 
 let main () =
-  let%bind _lines =
-    Reader.with_file "input" ~f:(fun r -> r |> Reader.lines |> Pipe.to_list)
-  in
-  printf "output\n";
+  let%bind game = Game.get () in
+  let game = { game with last_marble = game.last_marble * 100 } in
+  let scores = Game.simulate game in
+  Array.max_elt scores ~compare:Int.compare |> Option.value_exn |> printf "%d\n";
   return ()
 ;;
 
 let%expect_test "b" =
   let%bind () = main () in
-  [%expect {| output |}]
+  [%expect {| 3426843186 |}]
 ;;
