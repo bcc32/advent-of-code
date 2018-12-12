ab9c088389cdd0299736d0a1254bf9381916c620
Aaron L. Zeng <me@bcc32.com>
2018-12-12 13:48:40 -0500

    10b

--- 10/b.ml
+++ 10/b.ml
@@ -3,14 +3,23 @@ open! Async
 open! Import
 
 let main () =
-  let%bind _lines =
-    Reader.with_file "input" ~f:(fun r -> r |> Reader.lines |> Pipe.to_list)
+  let%bind points =
+    Reader.with_file "input" ~f:(fun r ->
+      r |> Reader.lines |> Pipe.map ~f:Point.of_string |> Pipe.to_list)
   in
-  printf "output\n";
+  let secs =
+    Points.to_sequence points
+    |> Sequence.fold_until
+         ~init:(Int.max_value, -1)
+         ~f:(fun (last_area, secs) (_, area) ->
+           if area > last_area then Stop secs else Continue (area, secs + 1))
+         ~finish:(fun _ -> assert false)
+  in
+  printf "%d\n" secs;
   return ()
 ;;
 
 let%expect_test "b" =
   let%bind () = main () in
-  [%expect {| output |}]
+  [%expect {| 10942 |}]
 ;;
