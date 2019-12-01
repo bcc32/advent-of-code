040a8b12a7e895256a9475634012783c4ba68b10
Aaron L. Zeng <me@bcc32.com>
2019-12-01 00:04:46 -0500

    Problem 01

--- 01/a.ml
+++ 01/a.ml
@@ -2,13 +2,18 @@ open! Core
 open! Async
 open! Import
 
+let fuel ~mass = (mass / 3) - 2
+
 let main () =
-  let%bind _lines = Reader.file_lines "input" in
-  printf "output\n";
+  let%bind lines = Reader.file_lines "input" in
+  lines
+  |> List.map ~f:Int.of_string
+  |> List.sum (module Int) ~f:(fun mass -> fuel ~mass)
+  |> printf "%d\n";
   return ()
 ;;
 
 let%expect_test "a" =
   let%bind () = main () in
-  [%expect {| output |}]
+  [%expect {| 3394032 |}]
 ;;
--- 01/b.ml
+++ 01/b.ml
@@ -2,13 +2,21 @@ open! Core
 open! Async
 open! Import
 
+let rec fuel ~mass =
+  let f = (mass / 3) - 2 in
+  if f <= 0 then 0 else f + fuel ~mass:f
+;;
+
 let main () =
-  let%bind _lines = Reader.file_lines "input" in
-  printf "output\n";
+  let%bind lines = Reader.file_lines "input" in
+  lines
+  |> List.map ~f:Int.of_string
+  |> List.sum (module Int) ~f:(fun mass -> fuel ~mass)
+  |> printf "%d\n";
   return ()
 ;;
 
 let%expect_test "b" =
   let%bind () = main () in
-  [%expect {| output |}]
+  [%expect {| 5088176 |}]
 ;;
