69bae204a4f496b06866d1651704ee93225d1972
Aaron L. Zeng <me@bcc32.com>
2019-11-30 23:58:52 -0500

    Use Reader.file_lines for input

--- template/a.ml
+++ template/a.ml
@@ -3,9 +3,7 @@ open! Async
 open! Import
 
 let main () =
-  let%bind _lines =
-    Reader.with_file "input" ~f:(fun r -> r |> Reader.lines |> Pipe.to_list)
-  in
+  let%bind _lines = Reader.file_lines "input" in
   printf "output\n";
   return ()
 ;;
