96a1c3f0be577394ff0d72df344d3522878569d6
Aaron L. Zeng <me@bcc32.com>
2019-12-09 17:21:40 -0500

    Intcode: refactor argument parsing and set instruction

--- intcode/program.ml
+++ intcode/program.ml
@@ -25,64 +25,63 @@ let get t ~arg ~mode =
   | _ -> assert false
 ;;
 
+let set t ~arg ~mode ~value =
+  match mode with
+  | 0 -> t.(arg) <- value
+  | 1 -> failwith "Cannot set using addressing mode 1"
+  | _ -> assert false
+;;
+
 let run t ~input ~output =
   let%bind () =
     Deferred.repeat_until_finished 0 (fun pc ->
-      let opcode, mode1, mode2, _mode3 = decode t.(pc) in
+      let opcode, mode1, mode2, mode3 = decode t.(pc) in
+      let with1 f =
+        let arg1 = t.(pc + 1) in
+        f ~x:(get t ~arg:arg1 ~mode:mode1)
+      in
+      let with2 f =
+        let arg1 = t.(pc + 1) in
+        let arg2 = t.(pc + 2) in
+        f ~x:(get t ~arg:arg1 ~mode:mode1) ~y:(get t ~arg:arg2 ~mode:mode2)
+      in
+      let with3 f =
+        let arg1 = t.(pc + 1) in
+        let arg2 = t.(pc + 2) in
+        let arg3 = t.(pc + 3) in
+        f ~x:(get t ~arg:arg1 ~mode:mode1) ~y:(get t ~arg:arg2 ~mode:mode2) ~arg3 ~mode3
+      in
       match opcode with
       | 1 ->
-        let arg1 = t.(pc + 1) in
-        let arg2 = t.(pc + 2) in
-        let x = get t ~arg:arg1 ~mode:mode1 in
-        let y = get t ~arg:arg2 ~mode:mode2 in
-        t.(t.(pc + 3)) <- x + y;
+        with3 (fun ~x ~y ~arg3 ~mode3 -> set t ~arg:arg3 ~mode:mode3 ~value:(x + y));
         return (`Repeat (pc + 4))
       | 2 ->
-        let arg1 = t.(pc + 1) in
-        let arg2 = t.(pc + 2) in
-        let x = get t ~arg:arg1 ~mode:mode1 in
-        let y = get t ~arg:arg2 ~mode:mode2 in
-        t.(t.(pc + 3)) <- x * y;
+        with3 (fun ~x ~y ~arg3 ~mode3 -> set t ~arg:arg3 ~mode:mode3 ~value:(x * y));
         return (`Repeat (pc + 4))
       | 3 ->
-        let addr = t.(pc + 1) in
+        let arg1 = t.(pc + 1) in
         (match%bind Pipe.read input with
          | `Eof -> raise_s [%message "t received EOF on input"]
          | `Ok input ->
-           t.(addr) <- input;
+           set t ~arg:arg1 ~mode:mode1 ~value:input;
            return (`Repeat (pc + 2)))
       | 4 ->
-        let arg1 = t.(pc + 1) in
-        let x = get t ~arg:arg1 ~mode:mode1 in
+        let x = with1 (fun ~x -> x) in
         let%bind () = Pipe.write output x in
         return (`Repeat (pc + 2))
       | 5 ->
-        let arg1 = t.(pc + 1) in
-        let arg2 = t.(pc + 2) in
-        let x = get t ~arg:arg1 ~mode:mode1 in
-        let y = get t ~arg:arg2 ~mode:mode2 in
-        if x <> 0 then return (`Repeat y) else return (`Repeat (pc + 3))
+        let pc = with2 (fun ~x ~y -> if x <> 0 then y else pc + 3) in
+        return (`Repeat pc)
       | 6 ->
-        let arg1 = t.(pc + 1) in
-        let arg2 = t.(pc + 2) in
-        let x = get t ~arg:arg1 ~mode:mode1 in
-        let y = get t ~arg:arg2 ~mode:mode2 in
-        if x = 0 then return (`Repeat y) else return (`Repeat (pc + 3))
+        let pc = with2 (fun ~x ~y -> if x = 0 then y else pc + 3) in
+        return (`Repeat pc)
       | 7 ->
-        let arg1 = t.(pc + 1) in
-        let arg2 = t.(pc + 2) in
-        let arg3 = t.(pc + 3) in
-        let x = get t ~arg:arg1 ~mode:mode1 in
-        let y = get t ~arg:arg2 ~mode:mode2 in
-        t.(arg3) <- Bool.to_int (x < y);
+        with3 (fun ~x ~y ~arg3 ~mode3 ->
+          set t ~arg:arg3 ~mode:mode3 ~value:(Bool.to_int (x < y)));
         return (`Repeat (pc + 4))
       | 8 ->
-        let arg1 = t.(pc + 1) in
-        let arg2 = t.(pc + 2) in
-        let arg3 = t.(pc + 3) in
-        let x = get t ~arg:arg1 ~mode:mode1 in
-        let y = get t ~arg:arg2 ~mode:mode2 in
-        t.(arg3) <- Bool.to_int (x = y);
+        with3 (fun ~x ~y ~arg3 ~mode3 ->
+          set t ~arg:arg3 ~mode:mode3 ~value:(Bool.to_int (x = y)));
         return (`Repeat (pc + 4))
       | 99 -> return (`Finished ())
       | code -> raise_s [%message "unrecognized opcode" (code : int)])
