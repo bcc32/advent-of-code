f6bd5b57afdd35ab02bd96de9b5911c4462e53cc
Aaron L. Zeng <me@bcc32.com>
2019-12-09 18:16:42 -0500

    09: simplify pipe logic

--- 09/main.ml
+++ 09/main.ml
@@ -7,12 +7,9 @@ let input () = Reader.file_contents "input" >>| Program.of_string
 
 let a () =
   let%bind program = input () in
-  let output =
-    Pipe.create_reader ~close_on_exception:true (fun writer ->
-      Program.run program ~input:(Pipe.of_list [ 1 ]) ~output:writer)
-  in
-  let%bind () = Pipe.iter_without_pushback output ~f:(printf "%d\n") in
-  return ()
+  Pipe.create_reader ~close_on_exception:true (fun writer ->
+    Program.run program ~input:(Pipe.of_list [ 1 ]) ~output:writer)
+  |> Pipe.iter_without_pushback ~f:(printf "%d\n")
 ;;
 
 let%expect_test "a" =
@@ -22,12 +19,9 @@ let%expect_test "a" =
 
 let b () =
   let%bind program = input () in
-  let output =
-    Pipe.create_reader ~close_on_exception:true (fun writer ->
-      Program.run program ~input:(Pipe.of_list [ 2 ]) ~output:writer)
-  in
-  let%bind () = Pipe.iter_without_pushback output ~f:(printf "%d\n") in
-  return ()
+  Pipe.create_reader ~close_on_exception:true (fun writer ->
+    Program.run program ~input:(Pipe.of_list [ 2 ]) ~output:writer)
+  |> Pipe.iter_without_pushback ~f:(printf "%d\n")
 ;;
 
 let%expect_test "b" =
