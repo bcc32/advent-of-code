a5b456faf1d53d6b61f28bc37d45d2fae61d0536
Aaron L. Zeng <me@bcc32.com>
2019-12-14 01:07:08 -0500

    14: Code cleanup

--- 14/main.ml
+++ 14/main.ml
@@ -12,10 +12,12 @@ module Reagent = struct
   [@@deriving compare, hash, sexp_of]
 end
 
-type rxn =
-  { inputs : Reagent.t list
-  ; output : Reagent.t
-  }
+module Reaction = struct
+  type t =
+    { inputs : Reagent.t list
+    ; output : Reagent.t
+    }
+end
 
 let parse_line =
   let rex =
@@ -30,29 +32,28 @@ let parse_line =
         let name = Re.Group.get group 2 in
         { Reagent.amt; name })
     in
-    let inputs = List.drop_last reagents |> uw in
+    let inputs = List.drop_last_exn reagents in
     let output = List.last_exn reagents in
-    { inputs; output }
+    { Reaction.inputs; output }
 ;;
 
 let input () =
   let%map lines = Reader.file_lines "input" in
-  let ways_to_make = String.Table.create () in
-  let reactions = List.map lines ~f:parse_line in
-  List.iter reactions ~f:(fun ({ inputs = _; output } as rxn) ->
-    Hashtbl.add_exn ways_to_make ~key:output.name ~data:rxn);
-  ways_to_make
+  lines
+  |> List.map ~f:parse_line
+  |> String.Table.create_with_key_exn ~get_key:(fun (reaction : Reaction.t) ->
+    reaction.output.name)
 ;;
 
-let ore_cost ~how_much_fuel ~ways_to_make =
+let ore_cost ~how_much_fuel ~(how_to_make : Reaction.t String.Table.t) =
   let nodes =
     Topological_sort.sort
       (module String)
-      (Hashtbl.keys ways_to_make)
-      (Hashtbl.to_alist ways_to_make
-       |> List.concat_map ~f:(fun (output, reaction) ->
+      (Hashtbl.keys how_to_make)
+      (Hashtbl.data how_to_make
+       |> List.concat_map ~f:(fun reaction ->
          List.map reaction.inputs ~f:(fun r ->
-           { Topological_sort.Edge.from = output; to_ = r.name })))
+           { Topological_sort.Edge.from = reaction.output.name; to_ = r.name })))
     |> ok_exn
   in
   let want_to_make = String.Table.of_alist_exn [ "FUEL", how_much_fuel ] in
@@ -62,22 +63,20 @@ let ore_cost ~how_much_fuel ~ways_to_make =
        | _ -> true))
     ~f:(fun need ->
       if debug then Debug.eprint_s [%message (want_to_make : (string, int) Hashtbl.t)];
-      let need_amt = Hashtbl.find_exn want_to_make need in
-      let rxn = Hashtbl.find_exn ways_to_make need in
+      let need_amt = Hashtbl.find_and_remove want_to_make need |> uw in
+      let rxn = Hashtbl.find_exn how_to_make need in
       let how_many_rxn =
         Int.round_up need_amt ~to_multiple_of:rxn.output.amt / rxn.output.amt
       in
       rxn.inputs
       |> List.iter ~f:(fun { amt; name } ->
-        Hashtbl.update want_to_make name ~f:(fun current_amt ->
-          Option.value current_amt ~default:0 + (amt * how_many_rxn)));
-      Hashtbl.remove want_to_make need);
-  Hashtbl.data want_to_make |> List.hd_exn
+        Hashtbl.incr want_to_make name ~by:(amt * how_many_rxn)));
+  Hashtbl.find_exn want_to_make "ORE"
 ;;
 
 let a () =
-  let%bind ways_to_make = input () in
-  ore_cost ~how_much_fuel:1 ~ways_to_make |> printf "%d\n";
+  let%bind how_to_make = input () in
+  ore_cost ~how_much_fuel:1 ~how_to_make |> printf "%d\n";
   return ()
 ;;
 
@@ -86,9 +85,9 @@ let%expect_test "a" =
   [%expect {| 654909 |}]
 ;;
 
-let how_much_fuel_can_we_make ~max_ore ~ways_to_make =
+let how_much_fuel_can_we_make ~max_ore ~how_to_make =
   let rec loop1 power_of_two =
-    if ore_cost ~how_much_fuel:power_of_two ~ways_to_make > max_ore
+    if ore_cost ~how_much_fuel:power_of_two ~how_to_make > max_ore
     then power_of_two
     else loop1 (power_of_two * 2)
   in
@@ -99,7 +98,7 @@ let how_much_fuel_can_we_make ~max_ore ~ways_to_make =
     then ubound - 1
     else (
       let midpoint = ubound - (pow2 / 2) in
-      if ore_cost ~how_much_fuel:midpoint ~ways_to_make > max_ore
+      if ore_cost ~how_much_fuel:midpoint ~how_to_make > max_ore
       then bisect midpoint (pow2 / 2)
       else bisect ubound (pow2 / 2))
   in
@@ -107,8 +106,8 @@ let how_much_fuel_can_we_make ~max_ore ~ways_to_make =
 ;;
 
 let b () =
-  let%bind ways_to_make = input () in
-  let max_fuel = how_much_fuel_can_we_make ~max_ore:1_000_000_000_000 ~ways_to_make in
+  let%bind how_to_make = input () in
+  let max_fuel = how_much_fuel_can_we_make ~max_ore:1_000_000_000_000 ~how_to_make in
   printf "%d\n" max_fuel;
   return ()
 ;;
