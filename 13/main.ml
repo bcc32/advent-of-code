4ad2b9e657ec71439193cd6085489fc67ee4bf8a
Aaron L. Zeng <me@bcc32.com>
2019-12-13 21:20:31 -0500

    13b: Use Mvar.t for latest-value semantics

--- 13/main.ml
+++ 13/main.ml
@@ -43,44 +43,29 @@ module Point = struct
   let score_loc = { x = -1; y = 0 }
 end
 
-let simulate program ~user_input ~on_game_changed ~on_waiting_for_input =
+let simulate program ~user_input ~on_game_changed =
   let tiles = Hashtbl.create (module Point) in
   let score = ref 0 in
   let%bind () = on_game_changed ~tiles ~score:!score in
-  match Program.run' program ~on_waiting_for_input with
-  | { input; output; done_ } ->
-    let%map () =
-      Deferred.repeat_until_finished () (fun () ->
-        match%bind Pipe.read_exactly output ~num_values:3 with
-        | `Eof -> return (`Finished ())
-        | `Fewer _ -> failwith "fewer"
-        | `Exactly q ->
-          let x = Queue.get q 0 in
-          let y = Queue.get q 1 in
-          let tile_id = Queue.get q 2 in
-          let key = { Point.x; y } in
-          let score_changed = ref false in
-          if [%equal: Point.t] key Point.score_loc
-          then (
-            score := tile_id;
-            score_changed := true)
-          else Hashtbl.set tiles ~key ~data:(Tile.of_int_exn tile_id);
-          let%bind () =
-            if !score_changed (* reached the end of batch *)
-            then on_game_changed ~tiles ~score:!score
-            else return ()
-          in
-          return (`Repeat ()))
-    and () =
-      let%map () =
-        Pipe.transfer user_input input ~f:(function
-          | `Left -> -1
-          | `Neutral -> 0
-          | `Right -> 1)
-      in
-      Pipe.close input
-    and () = done_ in
-    tiles
+  let output = Program.run' program ~input:user_input in
+  let%bind () =
+    Deferred.repeat_until_finished () (fun () ->
+      match%bind Pipe.read_exactly output ~num_values:3 with
+      | `Eof -> return (`Finished ())
+      | `Fewer _ -> failwith "fewer"
+      | `Exactly q ->
+        let x = Queue.get q 0 in
+        let y = Queue.get q 1 in
+        let tile_id = Queue.get q 2 in
+        let key = { Point.x; y } in
+        if [%equal: Point.t] key Point.score_loc
+        then score := tile_id
+        else Hashtbl.set tiles ~key ~data:(Tile.of_int_exn tile_id);
+        let%bind () = on_game_changed ~tiles ~score:!score in
+        return (`Repeat ()))
+  in
+  let%bind () = Pipe.closed output in
+  return tiles
 ;;
 
 (* TODO: Use [Pipe.empty] upon upgrading Async. *)
@@ -89,9 +74,8 @@ let a () =
   let%bind tiles =
     simulate
       program
-      ~user_input:(Pipe.of_list [])
+      ~user_input:(Mvar.create () |> Mvar.read_only)
       ~on_game_changed:(fun ~tiles:_ ~score:_ -> return ())
-      ~on_waiting_for_input:(fun () -> return ())
   in
   Hashtbl.count tiles ~f:([%equal: Tile.t] Block) |> printf "%d\n";
   return ()
@@ -105,9 +89,7 @@ let%expect_test "a" =
 let play ~user_input ~draw =
   let%bind program = input () in
   program.memory.(0) <- 2;
-  Deferred.ignore_m
-    (simulate program ~user_input ~on_game_changed:draw ~on_waiting_for_input:(fun () ->
-       return ()))
+  Deferred.ignore_m (simulate program ~user_input ~on_game_changed:draw)
 ;;
 
 let game_command =
@@ -116,13 +98,15 @@ let game_command =
     (let%map_open.Command () = return () in
      fun () ->
        let%bind term = Notty_async.Term.create () in
-       let user_input =
-         Pipe.filter_map (Notty_async.Term.events term) ~f:(function
-           | `Mouse _ | `Paste _ | `Resize _ -> None
-           | `Key (`Arrow `Left, _) -> Some `Left
-           | `Key (`Arrow `Down, _) -> Some `Neutral
-           | `Key (`Arrow `Right, _) -> Some `Right
-           | `Key _ -> None)
+       let user_input = Mvar.create () in
+       let () =
+         don't_wait_for
+           (Pipe.iter_without_pushback (Notty_async.Term.events term) ~f:(function
+              | `Mouse _ | `Paste _ | `Resize _ -> ()
+              | `Key (`Arrow `Left, _) -> Mvar.set user_input (-1)
+              | `Key (`Arrow `Down, _) -> Mvar.set user_input 0
+              | `Key (`Arrow `Right, _) -> Mvar.set user_input 1
+              | `Key _ -> ()))
        in
        let draw ~tiles ~score =
          if Hashtbl.is_empty tiles
@@ -152,7 +136,7 @@ let game_command =
            let image = Notty.I.(strf "Score: %d" score <-> image) in
            Notty_async.Term.image term image)
        in
-       play ~user_input ~draw)
+       play ~user_input:(Mvar.read_only user_input) ~draw)
 ;;
 
 let draw_plain ~tiles ~score =
@@ -188,17 +172,15 @@ let solve () =
   program.memory.(0) <- 2;
   let score = ref 0 in
   let last_tiles = ref (Hashtbl.create (module Point)) in
-  let user_input_r, user_input_w = Pipe.create () in
+  let user_input = Mvar.create () in
   let%bind tiles =
     simulate
       program
-      ~user_input:user_input_r
+      ~user_input:(Mvar.read_only user_input)
       ~on_game_changed:(fun ~tiles ~score:new_score ->
         score := new_score;
         last_tiles := tiles;
         if debug then draw_plain ~tiles ~score:!score;
-        return ())
-      ~on_waiting_for_input:(fun () ->
         let find_tile_col tile =
           with_return_option (fun { return } ->
             Hashtbl.iteri !last_tiles ~f:(fun ~key:{ x; y = _ } ~data:tile' ->
@@ -215,7 +197,13 @@ let solve () =
              | Equal -> `Neutral
              | Greater -> `Right)
         in
-        Pipe.write user_input_w joystick_action)
+        Mvar.set
+          user_input
+          (match joystick_action with
+           | `Left -> -1
+           | `Neutral -> 0
+           | `Right -> 1);
+        return ())
   in
   assert (not (Hashtbl.exists tiles ~f:([%equal: Tile.t] Block)));
   return !score
--- intcode/program.ml
+++ intcode/program.ml
@@ -71,65 +71,72 @@ let set t ~arg ~mode ~value =
   | _ -> assert false
 ;;
 
-let run' t ~on_waiting_for_input =
+let run_internal t ~get_input ~output =
+  Deferred.repeat_until_finished 0 (fun pc ->
+    let opcode, mode1, mode2, mode3 = decode t.memory.(pc) in
+    let with1 f =
+      let arg1 = t.memory.(pc + 1) in
+      f ~x:(get t ~arg:arg1 ~mode:mode1)
+    in
+    let with2 f =
+      let arg1 = t.memory.(pc + 1) in
+      let arg2 = t.memory.(pc + 2) in
+      f ~x:(get t ~arg:arg1 ~mode:mode1) ~y:(get t ~arg:arg2 ~mode:mode2)
+    in
+    let with3 f =
+      let arg1 = t.memory.(pc + 1) in
+      let arg2 = t.memory.(pc + 2) in
+      let arg3 = t.memory.(pc + 3) in
+      f ~x:(get t ~arg:arg1 ~mode:mode1) ~y:(get t ~arg:arg2 ~mode:mode2) ~arg3 ~mode3
+    in
+    match opcode with
+    | 1 ->
+      with3 (fun ~x ~y ~arg3 ~mode3 -> set t ~arg:arg3 ~mode:mode3 ~value:(x + y));
+      return (`Repeat (pc + 4))
+    | 2 ->
+      with3 (fun ~x ~y ~arg3 ~mode3 -> set t ~arg:arg3 ~mode:mode3 ~value:(x * y));
+      return (`Repeat (pc + 4))
+    | 3 ->
+      let arg1 = t.memory.(pc + 1) in
+      let%bind input = get_input () in
+      set t ~arg:arg1 ~mode:mode1 ~value:input;
+      return (`Repeat (pc + 2))
+    | 4 ->
+      let x = with1 (fun ~x -> x) in
+      let%bind () = output x in
+      return (`Repeat (pc + 2))
+    | 5 ->
+      let pc = with2 (fun ~x ~y -> if x <> 0 then y else pc + 3) in
+      return (`Repeat pc)
+    | 6 ->
+      let pc = with2 (fun ~x ~y -> if x = 0 then y else pc + 3) in
+      return (`Repeat pc)
+    | 7 ->
+      with3 (fun ~x ~y ~arg3 ~mode3 ->
+        set t ~arg:arg3 ~mode:mode3 ~value:(Bool.to_int (x < y)));
+      return (`Repeat (pc + 4))
+    | 8 ->
+      with3 (fun ~x ~y ~arg3 ~mode3 ->
+        set t ~arg:arg3 ~mode:mode3 ~value:(Bool.to_int (x = y)));
+      return (`Repeat (pc + 4))
+    | 9 ->
+      with1 (fun ~x -> t.relative_base <- t.relative_base + x);
+      return (`Repeat (pc + 2))
+    | 99 -> return (`Finished ())
+    | code -> raise_s [%message "unrecognized opcode" (code : int)])
+;;
+
+let run t =
   let input_r, input_w = Pipe.create () in
   let output_r, output_w = Pipe.create () in
   let done_ =
-    Deferred.repeat_until_finished 0 (fun pc ->
-      let opcode, mode1, mode2, mode3 = decode t.memory.(pc) in
-      let with1 f =
-        let arg1 = t.memory.(pc + 1) in
-        f ~x:(get t ~arg:arg1 ~mode:mode1)
-      in
-      let with2 f =
-        let arg1 = t.memory.(pc + 1) in
-        let arg2 = t.memory.(pc + 2) in
-        f ~x:(get t ~arg:arg1 ~mode:mode1) ~y:(get t ~arg:arg2 ~mode:mode2)
-      in
-      let with3 f =
-        let arg1 = t.memory.(pc + 1) in
-        let arg2 = t.memory.(pc + 2) in
-        let arg3 = t.memory.(pc + 3) in
-        f ~x:(get t ~arg:arg1 ~mode:mode1) ~y:(get t ~arg:arg2 ~mode:mode2) ~arg3 ~mode3
-      in
-      match opcode with
-      | 1 ->
-        with3 (fun ~x ~y ~arg3 ~mode3 -> set t ~arg:arg3 ~mode:mode3 ~value:(x + y));
-        return (`Repeat (pc + 4))
-      | 2 ->
-        with3 (fun ~x ~y ~arg3 ~mode3 -> set t ~arg:arg3 ~mode:mode3 ~value:(x * y));
-        return (`Repeat (pc + 4))
-      | 3 ->
-        let arg1 = t.memory.(pc + 1) in
-        let%bind () = on_waiting_for_input () in
-        (match%bind Pipe.read input_r with
-         | `Eof -> raise_s [%message "t received EOF on input"]
-         | `Ok input ->
-           set t ~arg:arg1 ~mode:mode1 ~value:input;
-           return (`Repeat (pc + 2)))
-      | 4 ->
-        let x = with1 (fun ~x -> x) in
-        let%bind () = Pipe.write output_w x in
-        return (`Repeat (pc + 2))
-      | 5 ->
-        let pc = with2 (fun ~x ~y -> if x <> 0 then y else pc + 3) in
-        return (`Repeat pc)
-      | 6 ->
-        let pc = with2 (fun ~x ~y -> if x = 0 then y else pc + 3) in
-        return (`Repeat pc)
-      | 7 ->
-        with3 (fun ~x ~y ~arg3 ~mode3 ->
-          set t ~arg:arg3 ~mode:mode3 ~value:(Bool.to_int (x < y)));
-        return (`Repeat (pc + 4))
-      | 8 ->
-        with3 (fun ~x ~y ~arg3 ~mode3 ->
-          set t ~arg:arg3 ~mode:mode3 ~value:(Bool.to_int (x = y)));
-        return (`Repeat (pc + 4))
-      | 9 ->
-        with1 (fun ~x -> t.relative_base <- t.relative_base + x);
-        return (`Repeat (pc + 2))
-      | 99 -> return (`Finished ())
-      | code -> raise_s [%message "unrecognized opcode" (code : int)])
+    run_internal
+      t
+      ~get_input:(fun () ->
+        match%map Pipe.read input_r with
+        | `Eof -> raise_s [%message "Program received EOF on input"]
+        | `Ok input -> input)
+      ~output:(Pipe.write output_w)
   in
   don't_wait_for
     (let%map () = done_ in
@@ -138,7 +145,16 @@ let run' t ~on_waiting_for_input =
   { Run.input = input_w; output = output_r; done_ }
 ;;
 
-let run t = run' t ~on_waiting_for_input:(fun () -> return ())
+let run' t ~input =
+  let output_r, output_w = Pipe.create () in
+  let done_ =
+    run_internal t ~get_input:(fun () -> Mvar.take input) ~output:(Pipe.write output_w)
+  in
+  don't_wait_for
+    (let%map () = done_ in
+     Pipe.close output_w);
+  output_r
+;;
 
 let run_without_io t =
   let { Run.input; output; done_ } = run t in
