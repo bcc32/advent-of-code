605aa71f555b9126ccd90d7b5ff572e694fb8c50
Aaron L. Zeng <me@bcc32.com>
2019-12-17 15:21:01 -0500

    Problem 16b

--- 16/main.ml
+++ 16/main.ml
@@ -34,12 +34,61 @@ let%expect_test "a" =
   [%expect {| 40921727 |}]
 ;;
 
+let do_phase_fast_only_correct_for_back_half_of_array_inplace array =
+  let sum = ref 0 in
+  for i = Array.length array - 1 downto 0 do
+    sum := !sum + array.(i);
+    array.(i) <- Int.abs (!sum mod 10)
+  done
+;;
+
+let value_at_index_is_correct_despite_incorrect_algorithm array i =
+  2 * i >= Array.length array - 1
+;;
+
+let%test_unit _ =
+  Base_quickcheck.Test.run_exn
+    (module struct
+      type t = int list [@@deriving quickcheck, sexp_of]
+    end)
+    ~f:(fun list ->
+      let array = Array.of_list list in
+      let expect = do_phase array in
+      let actual =
+        let array = Array.copy array in
+        do_phase_fast_only_correct_for_back_half_of_array_inplace array;
+        array
+      in
+      [%test_result: int] (Array.length actual) ~expect:(Array.length expect);
+      for i = 0 to Array.length actual - 1 do
+        if value_at_index_is_correct_despite_incorrect_algorithm actual i
+        then (
+          try [%test_result: int] actual.(i) ~expect:expect.(i) with
+          | exn -> Exn.reraisef exn !"value at index %d of %{sexp: int array}" i array ())
+      done)
+;;
+
 let b () =
-  let%bind _input = input () in
-  failwith "unimplemented"
+  let%bind input = input () in
+  let input = Array.concat (List.init 10_000 ~f:(Fn.const input)) in
+  let offset =
+    Array.sub input ~pos:0 ~len:7
+    |> Array.map ~f:Int.to_string
+    |> String.concat_array
+    |> Int.of_string
+  in
+  assert (value_at_index_is_correct_despite_incorrect_algorithm input offset);
+  for _ = 1 to 100 do
+    do_phase_fast_only_correct_for_back_half_of_array_inplace input
+  done;
+  Array.sub input ~pos:offset ~len:8
+  |> Array.map ~f:Int.to_string
+  |> String.concat_array
+  |> print_endline;
+  return ()
 ;;
 
 let%expect_test "b" =
-  let%bind () = show_raise_async b in
-  [%expect {| (raised (Failure unimplemented)) |}]
+  let%bind () = b () in
+  [%expect {| 89950138 |}]
 ;;
