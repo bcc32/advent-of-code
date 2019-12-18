c98632a387584866021618ad580baf1188f74e5f
Aaron L. Zeng <me@bcc32.com>
2019-12-18 01:42:14 -0500

    18b: Try using Dijkstra's algorithm and staged graph traversal

--- 18/main.ml
+++ 18/main.ml
@@ -34,7 +34,7 @@ module State = struct
     ; j : int
     ; collected_keys : Bitset.t
     }
-  [@@deriving compare, hash, sexp_of]
+  [@@deriving compare, equal, hash, sexp_of]
 end
 
 let a () =
@@ -96,7 +96,7 @@ module State' = struct
     ; br : int * int
     ; collected_keys : Bitset.t
     }
-  [@@deriving compare, fields, hash, sexp_of]
+  [@@deriving compare, equal, fields, hash, sexp_of]
 end
 
 let b () =
@@ -114,43 +114,81 @@ let b () =
     | _ -> assert false
   in
   let all_keys =
-    Array.fold grid ~init:Char.Set.empty ~f:(fun accum row ->
+    Array.fold grid ~init:Bitset.empty ~f:(fun accum row ->
       Array.fold row ~init:accum ~f:(fun accum x ->
         match x with
-        | Key c -> Set.add accum c
+        | Key c -> Bitset.add accum c
         | _ -> accum))
   in
-  let graph =
+  let graph_one_moves field initial_state =
     Graph.of_functions
       (module State')
       ~incoming_edges:(fun _ -> failwith "unimplemented")
-      ~outgoing_edges:(fun ({ tl; tr; bl; br; collected_keys } as state) ->
-        [ tl, State'.Fields.tl
-        ; tr, State'.Fields.tr
-        ; bl, State'.Fields.bl
-        ; br, State'.Fields.br
-        ]
-        |> List.concat_map ~f:(fun ((i, j), field) ->
+      ~outgoing_edges:(fun state ->
+        let i, j = Field.get field state in
+        let do_the_thing () =
           Robot.Dir.all
           |> List.map ~f:(fun d -> Robot.Point.add (i, j) d)
           |> List.filter_map ~f:(fun (i, j) ->
             match grid.(i).(j) with
             | exception _ -> None
-            | Door c
-              when Bitset.mem collected_keys (Char.to_int c - Char.to_int 'a')
-              -> Some (Field.fset field state (i, j))
+            | Door c when Bitset.mem state.collected_keys c ->
+              Some (Field.fset field state (i, j))
             | Wall | Door _ -> None
             | Empty | Entrance -> Some (Field.fset field state (i, j))
             | Key c ->
               Some
                 { (Field.fset field state (i, j)) with
-                  collected_keys =
-                    Bitset.add collected_keys (Char.to_int c - Char.to_int 'a')
-                })))
+                  collected_keys = Bitset.add state.collected_keys c
+                })
+        in
+        match grid.(i).(j) with
+        | Door _ | Wall | Empty | Entrance -> do_the_thing ()
+        | Key _ when [%equal: State'.t] state initial_state -> do_the_thing ()
+        | Key _ ->
+          (* don't move once we've hit a key *)
+          [])
   in
-  Graph.bfs graph ~start:start_state
-  |> Hashtbl.filter_keys ~f:(fun { collected_keys; _ } ->
-    Bitset.length collected_keys = Set.length all_keys)
+  let outgoing_edges_and_weights state =
+    [ State'.Fields.tl; State'.Fields.tr; State'.Fields.bl; State'.Fields.br ]
+    |> List.concat_map ~f:(fun field ->
+      let g = graph_one_moves field state in
+      let d = Graph.bfs g ~start:state in
+      Hashtbl.filter_keys d ~f:(fun state' ->
+        let i, j = Field.get field state' in
+        match grid.(i).(j) with
+        | Key _ -> true
+        | _ -> false)
+      |> Hashtbl.to_alist)
+  in
+  let distances =
+    let module HH = Hash_heap.Make (State') in
+    let d = Hashtbl.create (module State') in
+    let q = HH.create [%compare: int] in
+    HH.push_exn q ~key:start_state ~data:0;
+    let v = Hash_set.create (module State') in
+    Hashtbl.add_exn d ~key:start_state ~data:0;
+    while HH.length q > 0 do
+      let x, dx = HH.pop_with_key_exn q in
+      Hash_set.add v x;
+      outgoing_edges_and_weights x
+      |> List.iter ~f:(fun (y, w) ->
+        let dy = dx + w in
+        let dy =
+          match Hashtbl.find d y with
+          | None -> dy
+          | Some dy' -> Int.min dy dy'
+        in
+        Hashtbl.set d ~key:y ~data:dy;
+        if not (Hash_set.mem v y)
+        then (
+          HH.remove q y;
+          HH.push_exn q ~key:y ~data:dy))
+    done;
+    d
+  in
+  Hashtbl.filter_keys distances ~f:(fun state ->
+    Bitset.equal state.collected_keys all_keys)
   |> Hashtbl.data
   |> List.min_elt ~compare:[%compare: int]
   |> uw
