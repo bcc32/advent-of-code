6a70e5842b92e9f2620698de29ddc1914d34efa9
Aaron L. Zeng <me@bcc32.com>
2019-12-18 00:41:53 -0500

    Problem 18a

---
+++ 18/main.ml
@@ -0,0 +1,95 @@
+open! Core
+open! Async
+open! Import
+
+module Square = struct
+  type t =
+    | Wall
+    | Empty
+    | Key of char
+    | Door of char
+    | Entrance
+  [@@deriving equal]
+
+  let of_char_exn = function
+    | '#' -> Wall
+    | '.' -> Empty
+    | 'a' .. 'z' as lower -> Key lower
+    | 'A' .. 'Z' as upper -> Door (Char.lowercase upper)
+    | '@' -> Entrance
+    | c -> invalid_argf "Square.of_char_exn: %c" c ()
+  ;;
+end
+
+let parse_line line = line |> String.to_array |> Array.map ~f:Square.of_char_exn
+
+let input () =
+  let%map lines = Reader.file_lines "input" in
+  lines |> Array.of_list_map ~f:parse_line
+;;
+
+module State = struct
+  type t =
+    { i : int
+    ; j : int
+    ; collected_keys : Set.M(Char).t
+    }
+  [@@deriving compare, hash, sexp_of]
+end
+
+let a () =
+  let%bind grid = input () in
+  let i, j =
+    Array.find_mapi_exn grid ~f:(fun i row ->
+      Array.find_mapi row ~f:(fun j x ->
+        if [%equal: Square.t] x Entrance then Some j else None)
+      |> Option.map ~f:(fun j -> i, j))
+  in
+  let all_keys =
+    Array.fold grid ~init:Char.Set.empty ~f:(fun accum row ->
+      Array.fold row ~init:accum ~f:(fun accum x ->
+        match x with
+        | Key c -> Set.add accum c
+        | _ -> accum))
+  in
+  let graph =
+    Graph.of_functions
+      (module State)
+      ~incoming_edges:(fun _ -> failwith "unimplemented")
+      ~outgoing_edges:(fun { i; j; collected_keys } ->
+        Robot.Dir.all
+        |> List.map ~f:(fun d -> Robot.Point.add (i, j) d)
+        |> List.filter_map ~f:(fun (i, j) ->
+          match grid.(i).(j) with
+          | exception _ -> None
+          | Door c when Set.mem collected_keys c ->
+            Some { State.i; j; collected_keys }
+          | Wall | Door _ -> None
+          | Empty | Entrance -> Some { i; j; collected_keys }
+          | Key c -> Some { i; j; collected_keys = Set.add collected_keys c }))
+  in
+  Graph.bfs graph ~start:{ i; j; collected_keys = Char.Set.empty }
+  |> Hashtbl.filter_keys ~f:(fun { i = _; j = _; collected_keys } ->
+    [%equal: Char.Set.t] collected_keys all_keys)
+  |> Hashtbl.data
+  |> List.min_elt ~compare:[%compare: int]
+  |> uw
+  |> printf "%d\n";
+  return ()
+;;
+
+let%expect_test "a" =
+  let%bind () = a () in
+  [%expect {| 4700 |}]
+;;
+
+let b () =
+  let%bind _input = input () in
+  printf "output\n";
+  return ()
+;;
+
+let%expect_test "b" =
+  let%bind () = b () in
+  [%expect {| output |}]
+;;
