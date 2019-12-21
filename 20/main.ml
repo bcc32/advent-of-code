0a0c69306b3292079b1763c8e200d4bcd4db496e
Aaron L. Zeng <me@bcc32.com>
2019-12-20 22:58:36 -0500

    20: refactor and deduplicate code

--- 20/main.ml
+++ 20/main.ml
@@ -9,6 +9,20 @@ let input () =
   lines |> Array.of_list_map ~f:String.to_array
 ;;
 
+let distance_from_wall grid (i, j) =
+  [ i; j; Array.length grid - i - 1; Array.length grid.(0) - j - 1 ]
+  |> List.reduce_exn ~f:Int.min
+;;
+
+module Labeled_point = struct
+  type t =
+    { point : Robot.Point.t
+    ; label : string
+    ; which : [ `Inner | `Outer | `Only ]
+    }
+  [@@deriving compare, hash, sexp_of, equal]
+end
+
 let find_labels grid =
   let results = ref [] in
   (* horizontal *)
@@ -38,42 +52,69 @@ let find_labels grid =
     done
   done;
   !results
+  |> String.Table.of_alist_multi
+  |> Hashtbl.to_alist
+  |> List.concat_map ~f:(fun (label, data) ->
+    match data with
+    | [ point ] -> [ { Labeled_point.point; label; which = `Only } ]
+    | [ x; y ] ->
+      let outer_point, inner_point =
+        if distance_from_wall grid x < distance_from_wall grid y then x, y else y, x
+      in
+      [ { Labeled_point.point = inner_point; label; which = `Inner }
+      ; { Labeled_point.point = outer_point; label; which = `Outer }
+      ]
+    | points ->
+      failwithf
+        !"Multiple points with label %s: %{sexp: Robot.Point.t list}"
+        label
+        points
+        ())
+  |> Array.of_list
 ;;
 
 let a () =
   let%bind grid = input () in
-  let labels = find_labels grid in
-  let by_label = labels |> String.Table.of_alist_multi in
-  let by_point =
-    labels |> List.map ~f:Tuple2.swap |> Hashtbl.of_alist_exn (module Robot.Point)
+  let all_labeled_points = find_labels grid in
+  let all_labeled_points_index_by_point =
+    all_labeled_points
+    |> Array.mapi ~f:(fun i p -> p.point, i)
+    |> Array.to_list
+    |> Hashtbl.of_alist_exn (module Robot.Point)
   in
   let graph =
     Graph.of_functions
       (module Robot.Point)
       ~incoming_edges:(fun _ -> assert false)
-      ~outgoing_edges:(fun (i, j) ->
-        (Robot.Dir.all
-         |> List.map ~f:(fun d -> Robot.Point.add (i, j) d)
-         |> List.filter ~f:(fun (i, j) ->
-           match grid.(i).(j) with
-           | exception _ -> false
-           | '.' -> true
-           | _ -> false))
-        @
-        match Hashtbl.find by_point (i, j) with
-        | None -> []
-        | Some label ->
-          (match
-             Hashtbl.find_multi by_label label
-             |> List.filter ~f:(Fn.non ([%equal: int * int] (i, j)))
-           with
-           | [] -> []
-           | [ x ] -> [ x ]
-           | _ :: _ :: _ -> assert false))
+      ~outgoing_edges:(fun point ->
+        (match Hashtbl.find all_labeled_points_index_by_point point with
+         | None -> []
+         | Some i ->
+           let labeled_point = all_labeled_points.(i) in
+           (match labeled_point.which with
+            | `Inner ->
+              let point' = all_labeled_points.(i + 1) in
+              assert (String.( = ) labeled_point.label point'.label);
+              [ point'.point ]
+            | `Outer ->
+              let point' = all_labeled_points.(i - 1) in
+              assert (String.( = ) labeled_point.label point'.label);
+              [ point'.point ]
+            | `Only -> []))
+        @ (Robot.Dir.all
+           |> List.map ~f:(fun d -> Robot.Point.add point d)
+           |> List.filter ~f:(fun (i, j) ->
+             match grid.(i).(j) with
+             | exception _ -> false
+             | '.' -> true
+             | _ -> false)))
   in
-  let distance = Graph.bfs graph ~start:(Hashtbl.find_exn by_label "AA" |> List.hd_exn) in
-  Hashtbl.find_exn distance (Hashtbl.find_exn by_label "ZZ" |> List.hd_exn)
-  |> printf "%d\n";
+  let the_one_and_only_exn label =
+    Array.find_map_exn all_labeled_points ~f:(fun lp ->
+      Option.some_if (String.( = ) lp.label label) lp.point)
+  in
+  let distance = Graph.bfs graph ~start:(the_one_and_only_exn "AA") in
+  Hashtbl.find_exn distance (the_one_and_only_exn "ZZ") |> printf "%d\n";
   return ()
 ;;
 
@@ -81,27 +122,6 @@ let%expect_test "a" =
   let%bind () = a () in
   [%expect {| 602 |}]
 ;;
-
-module Inner_outer = struct
-  type 'a t =
-    { inner : 'a
-    ; outer : 'a
-    }
-end
-
-let distance_from_wall grid (i, j) =
-  [ i; j; Array.length grid - i - 1; Array.length grid.(0) - j - 1 ]
-  |> List.reduce_exn ~f:Int.min
-;;
-
-module Labeled_point = struct
-  type t =
-    { point : Robot.Point.t
-    ; label : string
-    ; which : [ `Inner | `Outer | `Only ]
-    }
-  [@@deriving compare, hash, sexp_of, equal]
-end
 
 let dijkstra
       (type node)
@@ -146,7 +166,7 @@ let dijkstra
 
 module State = struct
   type t =
-    { point : Labeled_point.t
+    { labeled_point_index : int
     ; level : int
     }
   [@@deriving compare, hash, sexp_of]
@@ -154,34 +174,7 @@ end
 
 let b () =
   let%bind grid = input () in
-  let labels = find_labels grid in
-  let by_label =
-    labels
-    |> String.Table.of_alist_multi
-    |> Hashtbl.map ~f:(function
-      | [ x ] -> `One x
-      | [ x; y ] ->
-        if distance_from_wall grid x < distance_from_wall grid y
-        then `Two { Inner_outer.outer = x; inner = y }
-        else `Two { Inner_outer.inner = x; outer = y }
-      | _ -> assert false)
-  in
-  let all_labeled_points =
-    Hashtbl.to_alist by_label
-    |> List.concat_map ~f:(fun (label, points) ->
-      match points with
-      | `One point -> [ { Labeled_point.point; label; which = `Only } ]
-      | `Two { inner; outer } ->
-        [ { Labeled_point.point = inner; label; which = `Inner }
-        ; { Labeled_point.point = outer; label; which = `Outer }
-        ])
-  in
-  let all_labeled_points_index_by_lp =
-    all_labeled_points
-    |> List.mapi ~f:(fun i p -> p, i)
-    |> Hashtbl.of_alist_exn (module Labeled_point)
-  in
-  let all_labeled_points = Array.of_list all_labeled_points in
+  let all_labeled_points = find_labels grid in
   let label_to_label_distance =
     let graph =
       Graph.of_functions
@@ -201,48 +194,41 @@ let b () =
       Array.map all_labeled_points ~f:(fun p2 -> Hashtbl.find distance p2.point))
   in
   let outgoing_edges_by_point_offset_by_level =
-    Memo.general
-      ~hashable:(Hashtbl.Hashable.of_key (module Labeled_point))
-      (fun point ->
-         let labeled_point_index = Hashtbl.find_exn all_labeled_points_index_by_lp point in
-         Array.append
-           (match point.which with
-            | `Inner ->
-              let point' = all_labeled_points.(labeled_point_index + 1) in
-              assert (String.( = ) point.label point'.label);
-              [| { State.point = point'; level = 1 }, 1 |]
-            | `Outer ->
-              let point' = all_labeled_points.(labeled_point_index - 1) in
-              assert (String.( = ) point.label point'.label);
-              [| { State.point = point'; level = -1 }, 1 |]
-            | `Only -> [||])
-           (label_to_label_distance.(labeled_point_index)
-            |> Array.filter_mapi ~f:(fun i distance ->
-              let next_point = all_labeled_points.(i) in
-              match distance with
-              | None -> None
-              | Some distance ->
-                Some ({ State.point = next_point; level = 0 }, distance))))
+    Memo.general ~hashable:Int.hashable (fun labeled_point_index ->
+      let point = all_labeled_points.(labeled_point_index) in
+      Array.append
+        (match point.which with
+         | `Inner ->
+           let point' = all_labeled_points.(labeled_point_index + 1) in
+           assert (String.( = ) point.label point'.label);
+           [| { State.labeled_point_index = labeled_point_index + 1; level = 1 }, 1 |]
+         | `Outer ->
+           let point' = all_labeled_points.(labeled_point_index - 1) in
+           assert (String.( = ) point.label point'.label);
+           [| { State.labeled_point_index = labeled_point_index - 1; level = -1 }, 1 |]
+         | `Only -> [||])
+        (label_to_label_distance.(labeled_point_index)
+         |> Array.filter_mapi ~f:(fun next_point_index distance ->
+           match distance with
+           | None -> None
+           | Some distance ->
+             Some
+               ( { State.labeled_point_index = next_point_index; level = 0 }
+               , distance ))))
+  in
+  let the_one_and_only_exn label =
+    Array.find_mapi_exn all_labeled_points ~f:(fun i x ->
+      Option.some_if (String.( = ) x.label label) i)
   in
   match
     dijkstra
       (module State)
-      ~start:
-        { point =
-            { point =
-                (Hashtbl.find_exn by_label "AA"
-                 |> function
-                 | `One x -> x
-                 | `Two _ -> assert false)
-            ; label = "AA"
-            ; which = `Only
-            }
-        ; level = 0
-        }
+      ~start:{ labeled_point_index = the_one_and_only_exn "AA"; level = 0 }
       ~is_end:(fun (state : State.t) ->
-        String.( = ) state.point.label "ZZ" && state.level = 0)
-      ~outgoing_edges:(fun { State.point; level } ->
-        outgoing_edges_by_point_offset_by_level point
+        String.( = ) all_labeled_points.(state.labeled_point_index).label "ZZ"
+        && state.level = 0)
+      ~outgoing_edges:(fun { State.labeled_point_index; level } ->
+        outgoing_edges_by_point_offset_by_level labeled_point_index
         |> Array.map
              ~f:
                (Tuple2.map_fst ~f:(fun (state : State.t) ->
