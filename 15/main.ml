bf620e6c442c51450f5bf08c26500f57788b1a32
Aaron L. Zeng <me@bcc32.com>
2019-12-22 23:12:44 -0500

    15: Run program synchronously

--- 15/main.ml
+++ 15/main.ml
@@ -30,59 +30,54 @@ end
 (* TODO: Add a way to represent programs persistently and immutably. *)
 
 let explore ~program =
-  match Program.Async.run program with
-  | { input; output; done_ = _ } ->
-    let robot = Robot.create_without_dir ~initial_loc:(0, 0) in
-    let grid = Hashtbl.create (module Robot.Point) in
-    let step dir =
-      let%bind () = Pipe.write input (int_of_dir dir) in
-      match%bind Pipe.read output with
-      | `Eof -> failwith "eof"
-      | `Ok result ->
-        let mat = Material.of_int_exn result in
-        Hashtbl.set grid ~key:(Robot.Point.add (Robot.loc robot) dir) ~data:mat;
-        (match mat with
-         | Wall -> return `Hit_wall
-         | Empty | Oxygen ->
-           Robot.step_dir robot ~dir;
-           return `Ok)
-    in
-    let step_exn dir =
-      match%map step dir with
-      | `Hit_wall -> failwith "hit a wall"
-      | `Ok -> ()
-    in
-    let follow_exn path = Deferred.List.iter path ~f:step_exn in
-    let unfollow_exn path = follow_exn (List.rev_map path ~f:Robot.Dir.opp) in
-    let bfs_queue = Queue.of_list [ 0, 0 ] in
-    let path = Hashtbl.of_alist_exn (module Robot.Point) [ (0, 0), [] ] in
-    let rec loop () =
-      if Queue.is_empty bfs_queue
-      then return ()
-      else (
-        let x = Queue.dequeue_exn bfs_queue in
-        let p = Hashtbl.find_exn path x in
-        let%bind () = follow_exn (List.rev p) in
-        let%bind () =
-          Robot.Dir.all
-          |> List.map ~f:(fun d -> d, Robot.Point.add x d)
-          |> Deferred.List.iter ~f:(fun (dir, y) ->
-            if Hashtbl.mem path y
-            then return ()
-            else (
-              match%bind step dir with
-              | `Hit_wall -> return ()
-              | `Ok ->
-                Hashtbl.add_exn path ~key:y ~data:(dir :: p);
-                Queue.enqueue bfs_queue y;
-                let%bind () = step_exn (Robot.Dir.opp dir) in
-                return ()))
-        in
-        let%bind () = unfollow_exn (List.rev p) in
-        loop ())
-    in
-    let%bind () = loop () in
-    return (grid, path)
+  let robot = Robot.create_without_dir ~initial_loc:(0, 0) in
+  let grid = Hashtbl.create (module Robot.Point) in
+  let step dir =
+    Program.Sync.provide_input program (int_of_dir dir);
+    match Program.Sync.step program with
+    | Done | Need_input -> failwith "expected output"
+    | Output result ->
+      let mat = Material.of_int_exn result in
+      Hashtbl.set grid ~key:(Robot.Point.add (Robot.loc robot) dir) ~data:mat;
+      (match mat with
+       | Wall -> `Hit_wall
+       | Empty | Oxygen ->
+         Robot.step_dir robot ~dir;
+         `Ok)
+  in
+  let step_exn dir =
+    match step dir with
+    | `Hit_wall -> failwith "hit a wall"
+    | `Ok -> ()
+  in
+  let follow_exn path = List.iter path ~f:step_exn in
+  let unfollow_exn path = follow_exn (List.rev_map path ~f:Robot.Dir.opp) in
+  let bfs_queue = Queue.of_list [ 0, 0 ] in
+  let path = Hashtbl.of_alist_exn (module Robot.Point) [ (0, 0), [] ] in
+  let rec loop () =
+    if Queue.is_empty bfs_queue
+    then ()
+    else (
+      let x = Queue.dequeue_exn bfs_queue in
+      let p = Hashtbl.find_exn path x in
+      follow_exn (List.rev p);
+      Robot.Dir.all
+      |> List.map ~f:(fun d -> d, Robot.Point.add x d)
+      |> List.iter ~f:(fun (dir, y) ->
+        if Hashtbl.mem path y
+        then ()
+        else (
+          match step dir with
+          | `Hit_wall -> ()
+          | `Ok ->
+            Hashtbl.add_exn path ~key:y ~data:(dir :: p);
+            Queue.enqueue bfs_queue y;
+            step_exn (Robot.Dir.opp dir)));
+      unfollow_exn (List.rev p);
+      loop ())
+  in
+  loop ();
+  grid, path
 ;;
 
 let find_oxy_exn grid =
@@ -93,7 +88,7 @@ let find_oxy_exn grid =
 
 let a () =
   let%bind program = input () in
-  let%bind grid, path = explore ~program in
+  let grid, path = explore ~program in
   let oxy_point = find_oxy_exn grid in
   let path = Hashtbl.find_exn path oxy_point in
   printf "%d\n" (List.length path);
@@ -123,7 +118,7 @@ let spread_oxygen grid start =
 
 let b () =
   let%bind program = input () in
-  let%bind grid, _path = explore ~program in
+  let grid, _path = explore ~program in
   let oxy_point = find_oxy_exn grid in
   let spread_time = spread_oxygen grid oxy_point in
   printf "%d\n" spread_time;
