e956288b1223f481c472544310b6da93f7d23d8c
Aaron L. Zeng <me@bcc32.com>
2019-12-23 22:46:38 -0500

    Robot: Use polymorphic variant to encode capabilities

--- robot/robot.ml
+++ robot/robot.ml
@@ -3,16 +3,13 @@ open! Import
 module Dir = Dir
 module Point = Point
 
-type with_dir
-type without_dir
-
-type 'a t =
+type _ t =
   | With_dir :
       { mutable dir : Dir.t
       ; mutable loc : Point.t
       }
-      -> with_dir t
-  | Without_dir : { mutable loc : Point.t } -> without_dir t
+      -> [> `dir ] t
+  | Without_dir : { mutable loc : Point.t } -> [ `dummy ] t
 [@@deriving sexp_of]
 
 let create_without_dir ~initial_loc:loc = Without_dir { loc }
@@ -22,8 +19,8 @@ let loc : type a. a t -> Point.t = function
   | With_dir { dir = _; loc } | Without_dir { loc } -> loc
 ;;
 
-let step_forward (With_dir t : with_dir t) = t.loc <- Point.add t.loc t.dir
-let step_backward (With_dir t : with_dir t) = t.loc <- Point.add t.loc (Dir.opp t.dir)
+let step_forward (With_dir t : [> `dir ] t) = t.loc <- Point.add t.loc t.dir
+let step_backward (With_dir t : [> `dir ] t) = t.loc <- Point.add t.loc (Dir.opp t.dir)
 
 let step_dir (type a) (t : a t) ~dir =
   match t with
@@ -31,5 +28,5 @@ let step_dir (type a) (t : a t) ~dir =
   | Without_dir t -> t.loc <- Point.add t.loc dir
 ;;
 
-let dir (With_dir t : with_dir t) = t.dir
-let turn (With_dir t : with_dir t) which_way = t.dir <- Dir.turn t.dir which_way
+let dir (With_dir t : [> `dir ] t) = t.dir
+let turn (With_dir t : [> `dir ] t) which_way = t.dir <- Dir.turn t.dir which_way
--- robot/robot.mli
+++ robot/robot.mli
@@ -3,15 +3,13 @@ open! Import
 module Dir = Dir
 module Point = Point
 
-type with_dir
-type without_dir
 type _ t [@@deriving sexp_of]
 
-val create_without_dir : initial_loc:Point.t -> without_dir t
-val create_with_dir : initial_loc:Point.t -> initial_dir:Dir.t -> with_dir t
+val create_without_dir : initial_loc:Point.t -> [ `dummy ] t
+val create_with_dir : initial_loc:Point.t -> initial_dir:Dir.t -> [ `dir ] t
 val loc : _ t -> Point.t
-val step_forward : with_dir t -> unit
-val step_backward : with_dir t -> unit
+val step_forward : [> `dir ] t -> unit
+val step_backward : [> `dir ] t -> unit
 val step_dir : _ t -> dir:Dir.t -> unit
-val dir : with_dir t -> Dir.t
-val turn : with_dir t -> [ `Left | `Right ] -> unit
+val dir : [> `dir ] t -> Dir.t
+val turn : [> `dir ] t -> [ `Left | `Right ] -> unit
