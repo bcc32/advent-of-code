0beacc6c45debfac0f008595b3f86ce383664493
Aaron L. Zeng <me@bcc32.com>
2019-12-25 23:17:02 -0500

    Problem 24b

--- 24/main.ml
+++ 24/main.ml
@@ -121,10 +121,9 @@ let input () =
     line
     |> String.to_array
     |> Array.map ~f:(function
-      | '.' -> false
+      | '.' | '?' -> false
       | '#' -> true
       | c -> failwithf "unrecognized char: %c" c ()))
-  |> Bitmap.of_grid
 ;;
 
 (* FIXME: Deduplicate from problem 12. *)
@@ -144,8 +143,8 @@ let find_cycle_length init ~equal ~step =
   offset, cycle_length
 ;;
 
-let%expect_test "a" =
-  let%bind bitmap = input () in
+let%expect_test "step" =
+  let%bind bitmap = input () >>| Bitmap.of_grid in
   printf !"%{Bitmap#hum}\n" bitmap;
   let%bind () = [%expect {|
     ..##.
@@ -189,7 +188,7 @@ let%expect_test "a" =
 ;;
 
 let a () =
-  let%bind bitmap = input () in
+  let%bind bitmap = input () >>| Bitmap.of_grid in
   let offset, cycle_length =
     find_cycle_length bitmap ~equal:[%equal: Bitmap.t] ~step:Bitmap.step
   in
@@ -214,13 +213,203 @@ let%expect_test "a" =
     19923473 |}]
 ;;
 
+module Position = struct
+  module Within_level = struct
+    (** A position within a level.
+
+        {v
+          ABCDE
+          FGHIJ
+          KLMNO
+          PQRST
+          UVWXY
+         v} *)
+    module T = struct
+      type t =
+        | A
+        | B
+        | C
+        | D
+        | E
+        | F
+        | G
+        | H
+        | I
+        | J
+        | K
+        | L
+        | M
+        | N
+        | O
+        | P
+        | Q
+        | R
+        | S
+        | T
+        | U
+        | V
+        | W
+        | X
+        | Y
+      [@@deriving compare, enumerate, sexp_of]
+
+      include (val Comparator.make ~compare ~sexp_of_t)
+    end
+
+    include T
+
+    let of_int_exn =
+      Map.find_exn
+        (List.mapi all ~f:(fun i pos -> i, pos) |> Map.of_alist_exn (module Int))
+    ;;
+
+    let to_int =
+      Map.find_exn (List.mapi all ~f:(fun i pos -> pos, i) |> Map.of_alist_exn (module T))
+    ;;
+
+    let down_exn t = of_int_exn (to_int t + 5)
+    let left_exn t = of_int_exn (to_int t - 1)
+    let right_exn t = of_int_exn (to_int t + 1)
+    let up_exn t = of_int_exn (to_int t - 5)
+
+    let adjacent_same_level = function
+      | M -> invalid_arg "adjacent_same_level"
+      | A -> [ B; F ]
+      | (B | C | D) as t -> [ left_exn t; down_exn t; right_exn t ]
+      | E -> [ D; J ]
+      | (F | K | P) as t -> [ up_exn t; right_exn t; down_exn t ]
+      | (G | I | Q | S) as t -> [ up_exn t; right_exn t; down_exn t; left_exn t ]
+      | H -> [ C; G; I ]
+      | (J | O | T) as t -> [ up_exn t; left_exn t; down_exn t ]
+      | L -> [ G; K; Q ]
+      | N -> [ I; O; S ]
+      | R -> [ Q; S; W ]
+      | U -> [ P; V ]
+      | (V | W | X) as t -> [ left_exn t; up_exn t; right_exn t ]
+      | Y -> [ X; T ]
+    ;;
+
+    let adjacent_inward_level = function
+      | M -> invalid_arg "adjacent_inward_level"
+      | A | B | C | D | E | F | G | I | J | K | O | P | Q | S | T | U | V | W | X | Y ->
+        []
+      | H -> [ A; B; C; D; E ]
+      | L -> [ A; F; K; P; U ]
+      | N -> [ E; J; O; T; Y ]
+      | R -> [ U; V; W; X; Y ]
+    ;;
+
+    let adjacent_outward_level = function
+      | M -> invalid_arg "adjacent_outward_level"
+      | G | H | I | L | N | Q | R | S -> []
+      | A -> [ L; H ]
+      | B | C | D -> [ H ]
+      | E -> [ H; N ]
+      | F | K | P -> [ L ]
+      | J | O | T -> [ N ]
+      | U -> [ L; R ]
+      | V | W | X -> [ R ]
+      | Y -> [ N; R ]
+    ;;
+  end
+
+  type t = int * Within_level.t [@@deriving compare, sexp_of]
+
+  include (val Comparator.make ~compare ~sexp_of_t)
+
+  let adjacent (level, within_level) =
+    (Within_level.adjacent_same_level within_level
+     |> List.map ~f:(fun within_level -> level, within_level))
+    @ (Within_level.adjacent_inward_level within_level
+       |> List.map ~f:(fun within_level -> level + 1, within_level))
+    @ (Within_level.adjacent_outward_level within_level
+       |> List.map ~f:(fun within_level -> level - 1, within_level))
+  ;;
+end
+
+let step bugs =
+  Set.elements bugs
+  |> List.concat_map ~f:Position.adjacent
+  |> Set.of_list (module Position)
+  |> Set.filter ~f:(fun pos ->
+    let is_bug = Set.mem bugs pos in
+    let neighbor_count = Position.adjacent pos |> List.count ~f:(Set.mem bugs) in
+    if is_bug then neighbor_count = 1 else neighbor_count = 1 || neighbor_count = 2)
+;;
+
+let%expect_test "adjacent" =
+  let test pos = print_s [%sexp (Position.adjacent pos : Position.t list)] in
+  test (0, S);
+  let%bind () = [%expect {|
+    ((0 N)
+     (0 T)
+     (0 X)
+     (0 R)) |}] in
+  test (1, G);
+  let%bind () = [%expect {|
+    ((1 B)
+     (1 H)
+     (1 L)
+     (1 F)) |}] in
+  test (1, D);
+  let%bind () = [%expect {|
+    ((1 C)
+     (1 I)
+     (1 E)
+     (0 H)) |}] in
+  test (1, E);
+  let%bind () = [%expect {|
+    ((1 D)
+     (1 J)
+     (0 H)
+     (0 N)) |}] in
+  test (0, N);
+  let%bind () =
+    [%expect
+      {|
+    ((0 I)
+     (0 O)
+     (0 S)
+     (1 E)
+     (1 J)
+     (1 O)
+     (1 T)
+     (1 Y)) |}]
+  in
+  test (1, N);
+  let%bind () =
+    [%expect
+      {|
+    ((1 I)
+     (1 O)
+     (1 S)
+     (2 E)
+     (2 J)
+     (2 O)
+     (2 T)
+     (2 Y)) |}]
+  in
+  return ()
+;;
+
 let b () =
-  let%bind _input = input () in
-  printf "output\n";
+  let%bind grid = input () in
+  let bugs =
+    Array.foldi
+      grid
+      ~init:(Set.empty (module Position))
+      ~f:(fun i accum row ->
+        Array.foldi row ~init:accum ~f:(fun j accum bool ->
+          if bool
+          then Set.add accum (0, Position.Within_level.of_int_exn ((5 * i) + j))
+          else accum))
+  in
+  let bugs = Fn.apply_n_times ~n:200 step bugs in
+  printf "%d\n" (Set.length bugs);
   return ()
 ;;
 
 let%expect_test "b" =
   let%bind () = b () in
-  [%expect {| output |}]
+  [%expect {| 1902 |}]
 ;;
