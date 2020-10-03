713cd7af86d1cb10c2bb0ac296d2c124e2b78887
Aaron L. Zeng <me@bcc32.com>
2020-10-03 17:56:44 -0400

    Download input as part of start.sh

--- start.sh
+++ start.sh
@@ -5,3 +5,4 @@ set -eu
 day=$1
 cp -R template "day_$day"
 sed -i "s/day_n_template/day_${day}_solution/" "day_$day/dune"
+curl --cookie cookies.txt "https://adventofcode.com/2015/day/$day/input" -o "day_$day/input.txt"
--- template/a.ml
+++ template/a.ml
@@ -3,6 +3,8 @@ open! Async
 open! Import
 
 let%expect_test _ =
-  print_endline "hello";
-  [%expect {| hello |}]
+  let%bind contents = Reader.file_contents "input.txt" in
+  print_string contents;
+  let%bind () = [%expect {| example |}] in
+  return ()
 ;;
