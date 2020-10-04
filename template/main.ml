9aa8d72e1199a55fb046a99b2291f5cfae4c38d5
Aaron L. Zeng <me@bcc32.com>
2020-10-03 21:40:22 -0400

    Update template

--- template/main.ml
+++ template/main.ml
@@ -2,16 +2,28 @@ open! Core
 open! Async
 open! Import
 
+let input = Lazy_deferred.create (fun () -> Reader.file_contents "input.txt")
+
+let a () =
+  let%bind input = Lazy_deferred.force_exn input in
+  print_s [%sexp (String.length input : int)];
+  return ()
+;;
+
 let%expect_test "a" =
-  let%bind contents = Reader.file_contents "input.txt" in
-  print_string contents;
-  let%bind () = [%expect {| example |}] in
+  let%bind () = a () in
+  let%bind () = [%expect {| 8 |}] in
+  return ()
+;;
+
+let b () =
+  let%bind input = Lazy_deferred.force_exn input in
+  print_s [%sexp (String.length input : int)];
   return ()
 ;;
 
 let%expect_test "b" =
-  let%bind contents = Reader.file_contents "input.txt" in
-  print_string contents;
-  let%bind () = [%expect {| example |}] in
+  let%bind () = b () in
+  let%bind () = [%expect {| 8 |}] in
   return ()
 ;;
