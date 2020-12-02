a61691acb87a1874224910ea9e70c9122b0d43d8
Aaron L. Zeng <me@bcc32.com>
2020-12-02 00:16:06 -0500

    Make start.sh idempotent

--- start.sh
+++ start.sh
@@ -3,6 +3,6 @@
 set -eu
 
 day=$1
-cp -R template "day_$day"
+cp -RT template "day_$day"
 sed -i "s/day_n_template/day_${day}_solution/" "day_$day/dune"
 curl --cookie cookies.txt "https://adventofcode.com/2020/day/$day/input" -o "day_$day/input.txt"
