2c8eaf487fb42981e826961597fb2fe9544e215c
Aaron L. Zeng <me@bcc32.com>
2020-12-04 00:29:40 -0500

    template: Refactor input to make code sharing easier

--- template/main.ml
+++ template/main.ml
@@ -2,28 +2,36 @@ open! Core
 open! Async
 open! Import
 
-let input = Lazy_deferred.create (fun () -> Reader.file_contents "input.txt")
+module Input = struct
+  type t = string [@@deriving sexp_of]
+
+  let parse input : t = input
+
+  let t : t Lazy_deferred.t =
+    Lazy_deferred.create (fun () -> Reader.file_contents "input.txt" >>| parse)
+  ;;
+end
 
 let a () =
-  let%bind input = Lazy_deferred.force_exn input in
-  print_s [%sexp (String.length input : int)];
+  let%bind input = Lazy_deferred.force_exn Input.t in
+  print_s [%sexp (input : Input.t)];
   return ()
 ;;
 
 let%expect_test "a" =
   let%bind () = a () in
-  let%bind () = [%expect {| 8 |}] in
+  let%bind () = [%expect {| "example\n" |}] in
   return ()
 ;;
 
 let b () =
-  let%bind input = Lazy_deferred.force_exn input in
-  print_s [%sexp (String.length input : int)];
+  let%bind input = Lazy_deferred.force_exn Input.t in
+  print_s [%sexp (input : Input.t)];
   return ()
 ;;
 
 let%expect_test "b" =
   let%bind () = b () in
-  let%bind () = [%expect {| 8 |}] in
+  let%bind () = [%expect {| "example\n" |}] in
   return ()
 ;;
