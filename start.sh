dd6ea77d7daf4aa7daeae1dca6ebc33be0e18561
Aaron L. Zeng <me@bcc32.com>
2020-12-11 00:36:56 -0500

    Zero-pad single-digit day numbers

--- start.sh
+++ start.sh
@@ -3,6 +3,7 @@
 set -eu
 
 day=$1
-cp -RT template "day_$day"
-sed -i "s/day_n_template/day_${day}_solution/" "day_$day/dune"
-curl --cookie cookies.txt "https://adventofcode.com/2020/day/$day/input" -o "day_$day/input.txt"
+day_for_filename=$(printf "day_%02d" "$day")
+cp -RT template "$day_for_filename"
+sed -i "s/day_n_template/${day_for_filename}_solution/" "${day_for_filename}/dune"
+curl --cookie cookies.txt "https://adventofcode.com/2020/day/$day/input" -o "${day_for_filename}/input.txt"
