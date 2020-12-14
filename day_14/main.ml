c25cbe6406094f5f9027daf8c94309e883c2f2f0
Aaron L. Zeng <me@bcc32.com>
2020-12-14 12:57:21 -0500

    Day 14: Refactor and use variant to represent mask bits

--- day_14/main.ml
+++ day_14/main.ml
@@ -2,15 +2,78 @@ open! Core
 open! Async
 open! Import
 
+let bit_length = 36
+
+module Mask = struct
+  module Digit = struct
+    type t =
+      | Floating
+      | Zero
+      | One
+    [@@deriving sexp_of]
+  end
+
+  type t = Digit.t array [@@deriving sexp_of]
+
+  let invariant t =
+    Invariant.invariant [%here] t [%sexp_of: t] (fun () ->
+      assert (Array.length t = bit_length))
+  ;;
+
+  (* It doesn't matter what this is because the program will always start with a
+     [mask =] instruction. *)
+  let dummy = Array.create ~len:bit_length Digit.Floating
+
+  let re =
+    let open Re in
+    repn (set "X01") 36 (Some 36)
+  ;;
+
+  let of_string string =
+    let bits =
+      string
+      |> String.to_array
+      |> Array.map ~f:(fun digit : Digit.t ->
+        match digit with
+        | 'X' -> Floating
+        | '0' -> Zero
+        | '1' -> One
+        | _ -> failwith "invalid digit")
+    in
+    (* Put in LSB-first order *)
+    Array.rev_inplace bits;
+    invariant bits;
+    bits
+  ;;
+end
+
+module Int_or_bits = struct
+  type t =
+    { int : int
+    ; bits : bool array
+    }
+  [@@deriving fields, sexp_of]
+
+  let of_string string =
+    let int = Int.of_string string in
+    let bits = Array.init bit_length ~f:(fun bit -> (int lsr bit) land 1 <> 0) in
+    { int; bits }
+  ;;
+
+  let of_bits bits =
+    let int =
+      Array.fold_right bits ~init:0 ~f:(fun bit accum -> (2 * accum) + Bool.to_int bit)
+    in
+    { int; bits }
+  ;;
+end
+
 module Instruction = struct
   type t =
-    | Mask of
-        { mask_mask : int
-        ; mask : int
-        }
+    | Mask of Mask.t
     | Assign of
-        { index : int
-        ; value : int
+        { index : Int_or_bits.t
+        ; value : Int_or_bits.t
         }
   [@@deriving sexp_of]
 
@@ -18,7 +81,7 @@ module Instruction = struct
     let pattern =
       let open Re in
       alt
-        [ group (seq [ str "mask = "; group (repn (set "X01") 36 (Some 36)) ])
+        [ group (seq [ str "mask = "; group Mask.re ])
         ; group (seq [ str "mem["; group (rep1 digit); str "] = "; group (rep1 digit) ])
         ]
       |> compile
@@ -26,20 +89,10 @@ module Instruction = struct
     fun line ->
       let g = Re.exec pattern line in
       if Re.Group.test g 1
-      then (
-        let mask_mask, mask =
-          Re.Group.get g 2
-          |> String.to_list
-          |> List.fold ~init:(0, 0) ~f:(fun (mask_mask, mask) -> function
-            | 'X' -> (2 * mask_mask) + 0, 2 * mask
-            | '1' -> (2 * mask_mask) + 1, (2 * mask) + 1
-            | '0' -> (2 * mask_mask) + 1, 2 * mask
-            | _ -> failwith "invalid digit")
-        in
-        Mask { mask_mask; mask })
+      then Re.Group.get g 2 |> Mask.of_string |> Mask
       else (
-        let index = Re.Group.get g 4 |> Int.of_string in
-        let value = Re.Group.get g 5 |> Int.of_string in
+        let index = Re.Group.get g 4 |> Int_or_bits.of_string in
+        let value = Re.Group.get g 5 |> Int_or_bits.of_string in
         Assign { index; value })
   ;;
 end
@@ -57,12 +110,19 @@ module Input = struct
 end
 
 let run mem (instructions : Input.t) =
-  let current_mask = ref (0, 0) in
+  let current_mask = ref Mask.dummy in
   List.iter instructions ~f:(function
-    | Mask { mask_mask; mask } -> current_mask := mask_mask, mask
+    | Mask mask -> current_mask := mask
     | Assign { index; value } ->
-      let mask_mask, mask = !current_mask in
-      let real_value = lnot mask_mask land value lor (mask_mask land mask) in
+      let index = index.int in
+      let real_value =
+        Array.map2_exn value.bits !current_mask ~f:(fun value_bit mask_bit ->
+          match mask_bit with
+          | Floating -> value_bit
+          | Zero -> false
+          | One -> true)
+        |> Int_or_bits.of_bits
+      in
       Hashtbl.set mem ~key:index ~data:real_value)
 ;;
 
@@ -70,7 +130,10 @@ let a () =
   let%bind instructions = Lazy_deferred.force_exn Input.t in
   let mem = Hashtbl.create (module Int) in
   run mem instructions;
-  Hashtbl.data mem |> List.sum (module Int) ~f:Fn.id |> [%sexp_of: int] |> print_s;
+  Hashtbl.data mem
+  |> List.sum (module Int) ~f:Int_or_bits.int
+  |> [%sexp_of: int]
+  |> print_s;
   return ()
 ;;
 
@@ -80,43 +143,38 @@ let%expect_test "a" =
   return ()
 ;;
 
-let iterate_matching_indices ~provided_index ~mask_mask ~mask ~f =
-  let get_bit int bit = (int lsr Int.(36 - 1 - bit)) land 1 <> 0 in
+let iterate_matching_indices ~(provided_index : Int_or_bits.t) ~(mask : Mask.t) ~f =
   let rec loop bit accum =
     if bit = 36
     then f accum
-    else if (* X *)
-      not (get_bit mask_mask bit)
-    then (
-      loop (bit + 1) ((2 * accum) + 0);
-      loop (bit + 1) ((2 * accum) + 1))
-    else if (* 1 *)
-      get_bit mask_mask bit && get_bit mask bit
-    then loop (bit + 1) ((2 * accum) + 1)
-    else (* 0 *)
-      loop (bit + 1) ((2 * accum) + if get_bit provided_index bit then 1 else 0)
+    else (
+      match mask.(bit) with
+      | Floating ->
+        loop (bit + 1) ((2 * accum) + 0);
+        loop (bit + 1) ((2 * accum) + 1)
+      | Zero -> loop (bit + 1) ((2 * accum) + Bool.to_int provided_index.bits.(bit))
+      | One -> loop (bit + 1) ((2 * accum) + 1))
   in
   loop 0 0
 ;;
 
 let run mem (instructions : Input.t) =
-  let current_mask = ref (0, 0) in
+  let current_mask = ref Mask.dummy in
   List.iter instructions ~f:(function
-    | Mask { mask_mask; mask } -> current_mask := mask_mask, mask
+    | Mask mask -> current_mask := mask
     | Assign { index; value } ->
-      let mask_mask, mask = !current_mask in
-      iterate_matching_indices
-        ~provided_index:(Int.of_int index)
-        ~mask_mask
-        ~mask
-        ~f:(fun i -> Hashtbl.set mem ~key:i ~data:value))
+      iterate_matching_indices ~provided_index:index ~mask:!current_mask ~f:(fun i ->
+        Hashtbl.set mem ~key:i ~data:value))
 ;;
 
 let b () =
   let%bind instructions = Lazy_deferred.force_exn Input.t in
   let mem = Hashtbl.create (module Int) in
   run mem instructions;
-  Hashtbl.data mem |> List.sum (module Int) ~f:Fn.id |> [%sexp_of: int] |> print_s;
+  Hashtbl.data mem
+  |> List.sum (module Int) ~f:Int_or_bits.int
+  |> [%sexp_of: int]
+  |> print_s;
   return ()
 ;;
 
