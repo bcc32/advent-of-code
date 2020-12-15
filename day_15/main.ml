3e9170dea62b5130884236dcf65fb26ba5f76bc4
Aaron L. Zeng <me@bcc32.com>
2020-12-15 12:23:53 -0500

    Day 15: Remove unneeded second part of sequence

--- day_15/main.ml
+++ day_15/main.ml
@@ -15,13 +15,12 @@ module Input = struct
 end
 
 let play numbers =
-  (* Build list of (number, most_recent_index) *)
   Sequence.unfold_with_and_finish
     (Sequence.of_list numbers)
     ~init:(0, Map.empty (module Int), None)
     ~running_step:(fun (i, most_recent_index, _last_number_most_recent_index) number ->
       Yield
-        ( (number, Map.find most_recent_index number)
+        ( number
         , ( i + 1
           , Map.set most_recent_index ~key:number ~data:i
           , Map.find most_recent_index number ) ))
@@ -30,13 +29,13 @@ let play numbers =
       match last_number_most_recent_index with
       | None ->
         Yield
-          ( (0, Map.find most_recent_index 0)
+          ( 0
           , (i + 1, Map.set most_recent_index ~key:0 ~data:i, Map.find most_recent_index 0)
           )
       | Some i' ->
         let diff = i - i' - 1 in
         Yield
-          ( (diff, Map.find most_recent_index diff)
+          ( diff
           , ( i + 1
             , Map.set most_recent_index ~key:diff ~data:i
             , Map.find most_recent_index diff ) ))
@@ -44,7 +43,7 @@ let play numbers =
 
 let a () =
   let%bind input = Lazy_deferred.force_exn Input.t in
-  let ans = play input |> Sequence.map ~f:fst |> Fn.flip Sequence.nth_exn 2019 in
+  let ans = play input |> Fn.flip Sequence.nth_exn 2019 in
   print_s [%sexp (ans : int)];
   return ()
 ;;
@@ -57,7 +56,7 @@ let%expect_test "a" =
 
 let b () =
   let%bind input = Lazy_deferred.force_exn Input.t in
-  let ans = play input |> Sequence.map ~f:fst |> Fn.flip Sequence.nth_exn 29999999 in
+  let ans = play input |> Fn.flip Sequence.nth_exn 29999999 in
   print_s [%sexp (ans : int)];
   return ()
 ;;
