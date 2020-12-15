8d1b06f15936d3381dd198ee30441654e079ef9d
Aaron L. Zeng <me@bcc32.com>
2020-12-15 00:24:29 -0500

    Day 15
    Part b is quite slow, takes about 40s to execute.

---
+++ day_15/main.ml
@@ -0,0 +1,69 @@
+open! Core
+open! Async
+open! Import
+
+module Input = struct
+  open! Advent_of_code_input_helpers
+
+  type t = int list [@@deriving sexp_of]
+
+  let parse input : t = input |> String.strip |> words ~sep:"," |> List.map ~f:stoi
+
+  let t : t Lazy_deferred.t =
+    Lazy_deferred.create (fun () -> Reader.file_contents "input.txt" >>| parse)
+  ;;
+end
+
+let play numbers =
+  (* Build list of (number, most_recent_index) *)
+  Sequence.unfold_with_and_finish
+    (Sequence.of_list numbers)
+    ~init:(0, Map.empty (module Int), None)
+    ~running_step:(fun (i, most_recent_index, _last_number_most_recent_index) number ->
+      Yield
+        ( (number, Map.find most_recent_index number)
+        , ( i + 1
+          , Map.set most_recent_index ~key:number ~data:i
+          , Map.find most_recent_index number ) ))
+    ~inner_finished:Fn.id
+    ~finishing_step:(fun (i, most_recent_index, last_number_most_recent_index) ->
+      match last_number_most_recent_index with
+      | None ->
+        Yield
+          ( (0, Map.find most_recent_index 0)
+          , (i + 1, Map.set most_recent_index ~key:0 ~data:i, Map.find most_recent_index 0)
+          )
+      | Some i' ->
+        let diff = i - i' - 1 in
+        Yield
+          ( (diff, Map.find most_recent_index diff)
+          , ( i + 1
+            , Map.set most_recent_index ~key:diff ~data:i
+            , Map.find most_recent_index diff ) ))
+;;
+
+let a () =
+  let%bind input = Lazy_deferred.force_exn Input.t in
+  let ans = play input |> Sequence.map ~f:fst |> Fn.flip Sequence.nth_exn 2019 in
+  print_s [%sexp (ans : int)];
+  return ()
+;;
+
+let%expect_test "a" =
+  let%bind () = a () in
+  let%bind () = [%expect {| 1238 |}] in
+  return ()
+;;
+
+let b () =
+  let%bind input = Lazy_deferred.force_exn Input.t in
+  let ans = play input |> Sequence.map ~f:fst |> Fn.flip Sequence.nth_exn 29999999 in
+  print_s [%sexp (ans : int)];
+  return ()
+;;
+
+let%expect_test "b" =
+  let%bind () = b () in
+  let%bind () = [%expect {| 3745954 |}] in
+  return ()
+;;
