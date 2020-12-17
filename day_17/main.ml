8ad246b4bb1b265e620ee36ba54ace8530a2d822
Aaron L. Zeng <me@bcc32.com>
2020-12-17 00:37:59 -0500

    Day 17 b

--- day_17/main.ml
+++ day_17/main.ml
@@ -75,25 +75,64 @@ let%expect_test "a" =
   return ()
 ;;
 
+let neighbors (x, y, z, w) =
+  let%bind.List dx = [ -1; 0; 1 ] in
+  let%bind.List dy = [ -1; 0; 1 ] in
+  let%bind.List dz = [ -1; 0; 1 ] in
+  let%bind.List dw = [ -1; 0; 1 ] in
+  if dx = 0 && dy = 0 && dz = 0 && dw = 0 then [] else [ x + dx, y + dy, z + dz, w + dw ]
+;;
+
+module Point4 = struct
+  type t = int * int * int * int [@@deriving compare, hash, sexp_of]
+
+  include (val Comparator.make ~compare ~sexp_of_t)
+end
+
+let simulate initial_grid ~n =
+  let active_points = Hash_set.create (module Point4) in
+  for x = 0 to Array.length initial_grid - 1 do
+    for y = 0 to Array.length initial_grid.(0) - 1 do
+      if initial_grid.(x).(y) then Hash_set.add active_points (x, y, 0, 0)
+    done
+  done;
+  let active_points = ref active_points in
+  for _ = 1 to n do
+    let new_active_points = Hash_set.create (module Point4) in
+    let neighbors_of_active_points =
+      !active_points
+      |> Hash_set.to_list
+      |> List.concat_map ~f:neighbors
+      |> Set.of_list (module Point4)
+    in
+    let points_to_check =
+      (!active_points |> Hash_set.to_list) @ (neighbors_of_active_points |> Set.to_list)
+      |> Set.stable_dedup_list (module Point4)
+    in
+    List.iter points_to_check ~f:(fun point ->
+      let neighbor_count =
+        List.count (neighbors point) ~f:(Hash_set.mem !active_points)
+      in
+      let should_be_active =
+        if Hash_set.mem !active_points point
+        then neighbor_count = 2 || neighbor_count = 3
+        else neighbor_count = 3
+      in
+      if should_be_active then Hash_set.add new_active_points point);
+    active_points := new_active_points
+  done;
+  !active_points
+;;
+
 let b () =
-  let%bind input = Lazy_deferred.force_exn Input.t in
-  print_s [%sexp (input : Input.t)];
+  let%bind initial_grid = Lazy_deferred.force_exn Input.t in
+  let active_points = simulate initial_grid ~n:6 in
+  print_s [%sexp (Hash_set.length active_points : int)];
   return ()
 ;;
 
 let%expect_test "b" =
   let%bind () = b () in
-  let%bind () =
-    [%expect
-      {|
-        ((true false true true false false false false)
-         (false true false true false true true false)
-         (true true true false false false false false)
-         (false false false false true true false true)
-         (true false false false false true true true)
-         (false true false true false true false false)
-         (false true true false false false true true)
-         (true false false true false true true true)) |}]
-  in
+  let%bind () = [%expect {| 1600 |}] in
   return ()
 ;;
