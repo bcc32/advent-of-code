1dfb71b34df2787aec0c3810af13f109417efce8
Aaron L. Zeng <me@bcc32.com>
2020-12-20 01:10:43 -0500

    Day 20: Clean up and pre-rotate tiles
    Turns out that just rotating the tiles was most of my runtime cost...
    Running the tests goes from costing about 30s to about 1s.

--- day_20/main.ml
+++ day_20/main.ml
@@ -2,6 +2,8 @@ open! Core
 open! Async
 open! Import
 
+let debug = false
+
 module Tile = struct
   open Grid.O
 
@@ -108,7 +110,7 @@ let%expect_test "all_rotations" =
 module Input = struct
   open! Advent_of_code_input_helpers
 
-  type t = Tile.t Int.Map.t [@@deriving sexp_of]
+  type t = Tile.t list Int.Map.t [@@deriving sexp_of]
 
   let parse input : t =
     input
@@ -123,7 +125,7 @@ module Input = struct
           Grid.of_matrix_exn
             (Array.of_list_map ~f:(fun line -> String.to_array line) tl)
         in
-        tile_number, grid
+        tile_number, Tile.all_rotations grid
       | [] -> failwith "parse")
     |> Int.Map.of_alist_exn
   ;;
@@ -132,24 +134,6 @@ module Input = struct
     Lazy_deferred.create (fun () -> Reader.file_contents "input.txt" >>| parse)
   ;;
 end
-
-let does_align image =
-  let open Grid.O in
-  Grid.Lines.rows image `Top_to_bottom `Left_to_right
-  |> Sequence.for_all ~f:(fun row_coords ->
-    Sequence.zip row_coords (Sequence.tl_eagerly_exn row_coords)
-    |> Sequence.for_all ~f:(fun (left, right) ->
-      Tile.does_align_horiz
-        ~left:(snd image.%(left))
-        ~right:(snd image.%(right))))
-  && Grid.Lines.cols image `Left_to_right `Top_to_bottom
-     |> Sequence.for_all ~f:(fun col_coords ->
-       Sequence.zip col_coords (Sequence.tl_eagerly_exn col_coords)
-       |> Sequence.for_all ~f:(fun (top, bottom) ->
-         Tile.does_align_vert
-           ~top:(snd image.%(top))
-           ~bottom:(snd image.%(bottom))))
-;;
 
 exception Done
 
@@ -161,23 +145,19 @@ let find_arrangement_exn tiles =
     Grid.init
       ~width:image_side_length_in_tiles
       ~height:image_side_length_in_tiles
-      ~f:(Fn.const (0, Map.data tiles |> List.hd_exn))
+      ~f:(Fn.const (0, Map.data tiles |> List.hd_exn |> List.hd_exn))
   in
   let used_tiles = Hash_set.create (module Int) in
   let open Grid.O in
   let rec loop row col =
-    if false
-    then
-      Debug.eprint_s
-        [%message (row : int) (col : int) ~grid:(Grid.map working ~f:fst : int Grid.t)];
     if row < image_side_length_in_tiles
     then
       if col < image_side_length_in_tiles
       then
-        Map.iteri tiles ~f:(fun ~key:id ~data:tile ->
+        Map.iteri tiles ~f:(fun ~key:id ~data:tiles ->
           if not (Hash_set.mem used_tiles id)
           then
-            List.iter (Tile.all_rotations tile) ~f:(fun tile ->
+            List.iter tiles ~f:(fun tile ->
               let does_align_left =
                 if col > 0
                 then
@@ -202,7 +182,7 @@ let find_arrangement_exn tiles =
                 working.%(Coord.RC.create ~row ~col) <- 0, tile;
                 Hash_set.remove used_tiles id)))
       else loop (row + 1) 0
-    else raise Done
+    else raise_notrace Done
   in
   try
     loop 0 0;
@@ -214,7 +194,7 @@ let find_arrangement_exn tiles =
 let a () =
   let%bind tiles = Lazy_deferred.force_exn Input.t in
   let working, image_side_length_in_tiles = find_arrangement_exn tiles in
-  print_s [%sexp (Grid.map working ~f:fst : int Grid.t)];
+  if debug then Debug.eprint_s [%sexp (Grid.map working ~f:fst : int Grid.t)];
   let corners =
     [ Coord.RC.create ~row:0 ~col:0
     ; Coord.RC.create ~row:0 ~col:(image_side_length_in_tiles - 1)
@@ -233,23 +213,7 @@ let a () =
 
 let%expect_test "a" =
   let%bind () = a () in
-  let%bind () =
-    [%expect
-      {|
-    ((1447 3041 2069 3457 1789 3943 2011 1409 1091 1549 1669 3221)
-     (3583 3329 1777 1319 2389 3833 1171 1031 2749 3019 2707 3821)
-     (1867 3347 3037 1933 2927 1499 1151 1481 3253 1489 3373 3769)
-     (1423 2467 2383 2203 1373 1451 3539 3533 1213 2381 3169 3931)
-     (3659 1123 2837 2267 1543 3167 1993 2879 1277 3637 3967 2417)
-     (1399 1019 3863 2521 2633 2309 2687 2861 1427 3299 1009 1039)
-     (2957 2027 2357 1103 2683 1129 1381 1973 3229 1693 3527 3433)
-     (3319 2113 3079 3209 1637 3271 3187 1663 3313 1699 3877 3919)
-     (3023 2851 3413 1063 2663 3631 3613 2699 3391 2371 3469 3307)
-     (2131 2411 2897 2857 2377 2971 1997 2711 2213 1459 1697 3491)
-     (1249 3089 2731 3001 3853 2243 3547 1831 1231 2789 3407 2153)
-     (1873 2621 1523 3607 1811 3739 2473 2441 2647 1601 3793 2029))
-    17712468069479 |}]
-  in
+  let%bind () = [%expect {| 17712468069479 |}] in
   return ()
 ;;
 
