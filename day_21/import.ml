8b647fa20a7d08fb2c34d2c1d9c3f2074ad5b341
Aaron L. Zeng <me@bcc32.com>
2020-12-23 00:42:58 -0500

    Day 21

---
+++ day_21/import.ml
@@ -0,0 +1,9 @@
+open! Core
+open! Async
+include Expect_test_helpers_core
+include Expect_test_helpers_async
+include Advent_of_code_lattice_geometry
+
+let ( << ) f g x = f (g x)
+let ( >> ) f g x = x |> f |> g
+let () = Backtrace.elide := false
