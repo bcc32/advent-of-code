b209a456529f8c0e556f1ab432078b0bcac032a3
Aaron L. Zeng <me@bcc32.com>
2021-12-03 16:25:21 -0500

    Day 10

---
+++ day_10/main.ml
@@ -0,0 +1,54 @@
+open! Core
+open! Async
+open! Import
+
+let rle seq ~equal =
+  let rec start seq accum =
+    match Sequence.next seq with
+    | None -> accum
+    | Some (hd, tl) -> keep tl accum hd 1
+  and keep seq accum x count =
+    match Sequence.next seq with
+    | Some (hd, tl) when equal x hd -> keep tl accum x (count + 1)
+    | None | Some _ -> start seq ((x, count) :: accum)
+  in
+  List.rev (start seq [])
+;;
+
+let look_and_say s =
+  let buf = Buffer.create 16 in
+  List.iter
+    (rle (Sequence.of_seq (Caml.String.to_seq s)) ~equal:Char.equal)
+    ~f:(fun (char, count) -> bprintf buf "%d%c" count char);
+  Buffer.contents buf
+;;
+
+let input =
+  Lazy_deferred.create (fun () -> Reader.file_contents "input.txt" >>| String.rstrip)
+;;
+
+let a () =
+  let%bind input = Lazy_deferred.force_exn input in
+  let ans = Fn.apply_n_times look_and_say ~n:40 input in
+  print_s [%sexp (String.length ans : int)];
+  return ()
+;;
+
+let%expect_test "a" =
+  let%bind () = a () in
+  let%bind () = [%expect {| 492982 |}] in
+  return ()
+;;
+
+let b () =
+  let%bind input = Lazy_deferred.force_exn input in
+  let ans = Fn.apply_n_times look_and_say ~n:50 input in
+  print_s [%sexp (String.length ans : int)];
+  return ()
+;;
+
+let%expect_test "b" =
+  let%bind () = b () in
+  let%bind () = [%expect {| 6989950 |}] in
+  return ()
+;;
