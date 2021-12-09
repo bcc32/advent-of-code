443685f825d4ec9e6e5738839b77f961dbdf2de2
Aaron L. Zeng <me@bcc32.com>
2021-12-09 00:12:16 -0500

    Day 9: Refactor

--- 09/sol2.rb
+++ 09/sol2.rb
@@ -1,21 +1,26 @@
 require 'set'
 
 input = File.open('input') do |f|
-  f.lines.map(&:chomp).map(&:chars).map { |row| row.map(&:to_i) }
+  f.lines.map do |line|
+    line.chomp.chars.map(&:to_i)
+  end
 end
 
-def dfs(input, visited, i, j)
-  return if visited.include?([i, j])
+def dfs(grid, visited, i, j)
+  return 0 if visited.include?([i, j])
 
+  count = 1
   visited << [i, j]
 
   [[-1, 0], [1, 0], [0, -1], [0, 1]].each do |di, dj|
-    if (0...input.size).include?(i + di) \
-       && (0...input[0].size).include?(j + dj) \
-       && input[i + di][j + dj] != 9
-      dfs(input, visited, i + di, j + dj)
+    if (0...grid.size).include?(i + di) \
+       && (0...grid[0].size).include?(j + dj) \
+       && grid[i + di][j + dj] != 9
+      count += dfs(grid, visited, i + di, j + dj)
     end
   end
+
+  count
 end
 
 basins_size = []
@@ -24,14 +29,9 @@ visited = Set.new
 input.each_with_index do |row, i|
   row.each_with_index do |cell, j|
     if cell != 9
-      before = visited.size
-      dfs(input, visited, i, j)
-      if visited.size > before
-        basins_size << visited.size - before
-      end
+      basins_size << dfs(input, visited, i, j)
     end
   end
 end
 
-basins_size.sort!
-p basins_size[-3, 3].reduce(&:*)
+p basins_size.max(3).reduce(&:*)
