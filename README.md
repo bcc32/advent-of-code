21a8c1419f551dd9725ed8a8b39c70b398d5107e
Aaron L. Zeng <me@bcc32.com>
2021-12-14 01:34:57 -0500

    Update commentary and README.md

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # advent-of-code.el
+
+This package provides some convenient features for solving Advent of
+Code problems quickly.
+
+Press `C-c C-c` in an input buffer (`aoc.in`) to re-download the
+actual input assigned to you, in case you edited the file for
+debugging purposes.
+
+After the output buffer (`aoc.out`) is written to and auto-reverted by
+Emacs, its contents are automatically copied into the kill ring (and
+the X clipboard, if supported).
--- advent-of-code.el
+++ advent-of-code.el
@@ -9,12 +9,12 @@
 ;; This package provides some convenient features for solving Advent of Code
 ;; problems quickly.
 ;;
-;; Press C-c C-c in an input buffer to re-download the actual input assigned to
-;; you, in case you edited the file for debugging purposes.
+;; Press C-c C-c in an input buffer ("aoc.in") to re-download the actual input
+;; assigned to you, in case you edited the file for debugging purposes.
 ;;
-;; After the output buffer is written to and auto-reverted by Emacs, its
-;; contents are automatically copied into the kill ring (and the X clipboard, if
-;; supported).
+;; After the output buffer ("aoc.out") is written to and auto-reverted by Emacs,
+;; its contents are automatically copied into the kill ring (and the X
+;; clipboard, if supported).
 ;;
 ;;; Code:
 
