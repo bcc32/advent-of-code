51981932c1d0477b7ed61db2cee3d9fc33c60bce
Aaron L. Zeng <me@bcc32.com>
2021-12-15 01:42:21 -0500

    Day 15: Refactor

--- 15/sol1.rb
+++ 15/sol1.rb
@@ -9,13 +9,11 @@ def dijk(grid, start, end_)
   until queue.empty?
     x, y = queue.min_by(&dist)
     d = dist[[x, y]]
-    if [x, y] == end_
-      return d
-    end
+    return d if [x, y] == end_
     queue.delete([x, y])
     [[-1, 0], [1, 0], [0, -1], [0, 1]].each do |dx, dy|
       next unless (0...grid.size) === x + dx && (0...grid[0].size) === y + dy
-      next if dist.include?([x + dx, y + dy])
+      next if dist.has_key?([x + dx, y + dy])
       cost = grid[x + dx] && grid[x + dx][y + dy]
       dist[[x + dx, y + dy]] = d + cost
       queue << [x + dx, y + dy]
--- 15/sol2.rb
+++ 15/sol2.rb
@@ -18,22 +18,17 @@ end
 def dijk(grid, xmax, ymax, start, end_)
   dist = {}
   dist[start] = 0
-  pqueue = MinHeap.new { |x, y|
-    dist[x] <=> dist[y]
-  }
-  pqueue.push(start)
+  pqueue = MinHeap.new([start]) { |x, y| dist[x] <=> dist[y] }
   until pqueue.empty?
     x, y = pqueue.shift
     d = dist[[x, y]]
-    if [x, y] == end_
-      return d
-    end
+    return d if [x, y] == end_
     [[-1, 0], [1, 0], [0, -1], [0, 1]].each do |dx, dy|
       next unless (0..xmax) === x + dx && (0..ymax) === y + dy
-      next if dist.include?([x + dx, y + dy])
+      next if dist.has_key?([x + dx, y + dy])
       cost = cost(grid, x + dx, y + dy)
       dist[[x + dx, y + dy]] = d + cost
-      pqueue.push [x + dx, y + dy]
+      pqueue << [x + dx, y + dy]
     end
   end
 end
