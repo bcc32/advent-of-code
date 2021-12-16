3af3ce33929c5bb31d03260f489d436c74d985b4
Aaron L. Zeng <me@bcc32.com>
2021-12-16 01:41:34 -0500

    Consistently use aoc.in as name of input file

--- start.sh
+++ start.sh
@@ -9,4 +9,4 @@ printf -v dir "$year/%02d" "$day"
 mkdir -p "$dir"
 
 cp -nR templates/ruby/* -t "$dir"
-curl -fsSL --cookie cookies.txt "https://adventofcode.com/$year/day/$day/input" -o "$dir/input"
+curl -fsSL --cookie cookies.txt "https://adventofcode.com/$year/day/$day/input" -o "$dir/aoc.in"
