a87fd152819149b435203120fbf9f80226aa001c
Aaron L. Zeng <me@bcc32.com>
2021-12-20 00:19:25 -0500

    Day 20

---
+++ 2021/20/sol1.rb
@@ -0,0 +1,39 @@
+alg, grid = File.open('aoc.in') do |f|
+  alg = f.readline.chomp
+  f.readline
+  grid = f.readlines.map(&:chomp)
+  [alg, grid]
+end
+
+def tick(alg, grid, flip)
+  (-2..(grid.size+1)).map do |x|
+    (-2..(grid[0].size+1)).map do |y|
+      bits = []
+      (-1..1).each do |dx|
+        (-1..1).each do |dy|
+          if (0...grid.size) === x + dx && (0...grid[0].size) === y + dy
+            bits << (grid[x + dx][y + dy] == '#' ? 1 : 0)
+          else
+            bits << (flip ? 1 : 0)
+          end
+        end
+      end
+
+      index = bits.join.to_i(2)
+      alg[index]
+    end
+  end
+end
+
+# if flip, then out-of-bounds pixels are lit, else dark
+flip = false
+2.times do
+  grid = tick(alg, grid, flip)
+  if alg[0] == '#'
+    flip = !flip
+  end
+  puts grid.map(&:join).join("\n")
+end
+
+puts grid.map(&:join).join("\n")
+p grid.flatten.count('#')
