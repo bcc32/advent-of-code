6812f34a9358531d7907292106af0c98ea0cd220
Aaron L. Zeng <me@bcc32.com>
2023-12-24 02:35:03 -0500

    Make 2017 solutions build again

---
+++ 2017/.dir-locals.el
@@ -0,0 +1,5 @@
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
+((advent-of-code-input-mode . ((advent-of-code-year . 2017)))
+ (advent-of-code-output-mode . ((advent-of-code-year . 2017))))
